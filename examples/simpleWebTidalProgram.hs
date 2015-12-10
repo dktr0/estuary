5329c29e05498d0adb56a4dbe6d82a7a46bd3acd
d0kt0r0 <ogbornd@mcmaster.ca>
2015-12-10 11:23:01 -0500

    division of labour

--- examples/simpleWebTidalProgram.hs
+++ examples/simpleWebTidalProgram.hs
@@ -1,10 +1,9 @@
 import Sound.Tidal.Context
 import My.Cool.WebFramework
 
-playPattern stream pattern = stream $ sound (p pattern)
-
 main = do
-  d1 <- dirtStream {- but this will be on server side only -}
-  s <- getStringFromBrowserWidgetWhenTheyPressEnter
-  playPattern d1 s {- playPattern gets done when s "fires" -} 
+  s <- getStringFromBrowserWidgetWhenTheyPressEnter {- Matt -}
+  let x = p s {- translate string into a Pattern}
+  doSomethingCoolInBrowserWithPatternData x {- Matt -}
+  maybeAlsoSendThePatternToTheServerButDavidIsWorkingOnThatNow s {- David -}
   ()
