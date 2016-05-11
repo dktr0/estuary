ef7056d8d2843809518f588727aee6f67e6a3a07
d0kt0r0 <ogbornd@mcmaster.ca>
2016-05-11 12:35:39 -0400

    added insertIntoPatternAt function to SoundPattern

--- Estuary/SoundPattern.hs
+++ Estuary/SoundPattern.hs
@@ -7,3 +7,9 @@ data SoundPattern = SoundPattern [Sound] deriving (Eq)
 
 instance Show SoundPattern where
   show (SoundPattern xs) = intercalate " " (map show xs)
+
+emptyPattern :: SoundPattern
+emptyPattern = SoundPattern []
+
+insertIntoPatternAt :: Sound -> SoundPattern -> Int -> SoundPattern
+insertIntoPatternAt sound (SoundPattern pattern) position =  SoundPattern ((take position pattern) ++ [sound] ++ (drop position pattern))
