ec523083936c275f8f30f9622a948ca385aca9e8
Gscale-mbp <gscale@Gscale-mbps-MacBook-Pro.local>
2016-05-24 16:52:37 -0400

    Switched to use Maps

--- Estuary/Types/SoundPattern.hs
+++ Estuary/Types/SoundPattern.hs
@@ -1,26 +1,33 @@
 module Types.SoundPattern where
 
 import Types.Sound
-import Data.List (intercalate)
+import Data.Map as Map
 
-data SoundPattern = SoundPattern [Sound] deriving (Eq)
+data SoundPattern = SoundPattern (Map Int Sound) deriving (Eq)
 
-instance Show SoundPattern where
-  show (SoundPattern xs) = intercalate " " (map show xs)
+-- Rewrite
+--instance Show SoundPattern where
+  --show (SoundPattern xs) = intercalate " " (map show xs)
 
 initialContainer :: SoundPattern
 initialContainer = simple "sn"
 
-insert :: (Sound,Int) -> SoundPattern -> SoundPattern
-insert (sound,position) (SoundPattern pattern) =  SoundPattern ((take position pattern) ++ [sound] ++ (drop position pattern))
+insert :: (Int,Int,Sound) -> SoundPattern -> SoundPattern
+insert (dkey,ikey,sound) (SoundPattern pattern) = if Map.member (ikey+1) pattern
+                                                  then do
+                                                       let upattern = if (dkey == ikey) then pattern else Map.delete dkey pattern
+                                                       let (lp,gp) = Map.partitionWithKey (\ k _ -> k > (ikey+1)) upattern
+                                                       let gpp = Map.mapKeys (+1) gp
+                                                       let lpp = Map.insert ((+1) $ fst $ Map.findMax lp) sound pattern
+                                                       SoundPattern (Map.union lpp gpp)
+                                                  else SoundPattern (Map.insert (ikey+1) sound pattern)
 
 delete :: Int -> SoundPattern -> SoundPattern
-delete position (SoundPattern pattern) = SoundPattern ((take position pattern) ++ (drop (position+1) pattern))
+delete key (SoundPattern pattern) = SoundPattern (Map.delete key pattern)
 
 -- convenience functions
-
 empty :: SoundPattern
-empty = SoundPattern []
+empty = SoundPattern Map.empty
 
 simple :: String -> SoundPattern
-simple x = SoundPattern [simpleSound x]
+simple x = SoundPattern (Map.singleton 1 (simpleSound x))
