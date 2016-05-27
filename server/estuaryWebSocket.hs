4df9e3a3663f179b9256fdb93c8c4d78d78e3781
d0kt0r0 <ogbornd@mcmaster.ca>
2016-05-27 14:23:47 -0400

    estuaryWebSocket now silences layers for a given connection when connection is lost/dropped/stopped

--- server/estuaryWebSocket.hs
+++ server/estuaryWebSocket.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Control.Concurrent (MVar, newMVar)
 import Control.Monad (forever)
+import Control.Exception (try)
 import qualified Sound.Tidal.Context as Tidal
 import qualified Sound.Tidal.Stream as Tidal
 import Data.Text (Text)
@@ -16,44 +17,42 @@ import Text.JSON
 import Request
 import WebDirt
 
-type Client = WS.Connection
-
-type ServerState = [Client]
-
-newServerState :: ServerState
-newServerState = []
+type TidalState = (Double -> IO (),[Tidal.ParamPattern -> IO()])
 
 main = do
-  putStrLn "Tidal websocket server for Estuary"
-  state <- newMVar newServerState
-  WS.runServer "0.0.0.0" 9162 $ app state
+  putStrLn "Tidal websocket server for Estuary, listening on port 9162"
+  WS.runServer "0.0.0.0" 9162 $ (\pending -> do
+    conn <- WS.acceptRequest pending
+    putStrLn "received new connection"
+    WS.forkPingThread conn 30
+    (cps,getNow) <- Tidal.bpsUtils
+    dss <- mapM (\_ -> Tidal.dirtStream) [0..8]
+    loop (cps,dss) conn
+    )
 
-app :: MVar ServerState -> WS.ServerApp
-app state pending = do
-  conn <- WS.acceptRequest pending
-  WS.forkPingThread conn 30
-  Main.interact conn state
+loop :: TidalState -> WS.Connection -> IO ()
+loop s@(cps,dss) conn = do
+  m <- try (WS.receiveData conn)
+  case m of
+    Right x -> do
+      processResult s (decode (T.unpack x))
+      loop s conn
+    Left WS.ConnectionClosed -> close s "unexpected loss of connection"
+    Left (WS.CloseRequest _ _) -> close s "by request from peer"
+    Left (WS.ParseException e) -> close s ("parse exception: " ++ e)
 
-hintParamPattern  :: String -> IO (Either InterpreterError Tidal.ParamPattern)
-hintParamPattern x = Hint.runInterpreter $ do
-  Hint.set [languageExtensions := [OverloadedStrings]]
-  Hint.setImports ["Prelude","Sound.Tidal.Context","Sound.OSC.Type","Sound.OSC.Datum","Data.Map"]
-  Hint.interpret x (Hint.as::Tidal.ParamPattern)
+close :: TidalState -> String -> IO ()
+close (cps,dss) msg = do
+  putStrLn ("connection closed: " ++ msg)
+  hush dss
 
-interact :: WS.Connection -> MVar ServerState -> IO ()
-interact conn state = do
-  (cps,getNow) <- Tidal.bpsUtils
-  dss <- mapM (\_ -> Tidal.dirtStream) [0..8]
-  --  (d1, _) <- Tidal.superDirtSetters getNow
-  forever $ do
-    msg <- WS.receiveData conn
-    case decode (T.unpack msg) of (Error e) -> putStrLn ("Error: " ++ e)
-                                  (Ok request) -> do
-                                    putStrLn (show request)
-                                    respond (cps,dss) request
+processResult :: TidalState -> Result Request -> IO ()
+processResult _ (Error e) = putStrLn ("Error: " ++ e)
+processResult state (Ok request) = do
+  putStrLn (show request)
+  respond state request
 
-
-respond :: (Double -> IO (),[Tidal.ParamPattern -> IO()]) -> Request -> IO ()
+respond :: TidalState -> Request -> IO ()
 respond _ (Info i) = return ()
 respond (_,dss) Hush = mapM_ ($ Tidal.silence) dss
 respond (cps,_) (Cps x) = cps x
@@ -65,3 +64,11 @@ respond _ (Render patt cps cycles) = do
   x <- hintParamPattern patt
   case x of (Left error) -> putStrLn "Error interpreting pattern"
             (Right paramPattern) -> putStrLn (encode (render paramPattern cps cycles))
+
+hintParamPattern  :: String -> IO (Either InterpreterError Tidal.ParamPattern)
+hintParamPattern x = Hint.runInterpreter $ do
+  Hint.set [languageExtensions := [OverloadedStrings]]
+  Hint.setImports ["Prelude","Sound.Tidal.Context","Sound.OSC.Type","Sound.OSC.Datum","Data.Map"]
+  Hint.interpret x (Hint.as::Tidal.ParamPattern)
+
+hush = mapM_ ($ Tidal.silence)
