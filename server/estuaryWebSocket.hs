b61c846e3957b86c8012b1094e0ae06dfea67686
d0kt0r0 <ogbornd@mcmaster.ca>
2016-05-28 11:22:35 -0400

    rendered WebDirt scores come back to the browser client and are played and displayed

--- server/estuaryWebSocket.hs
+++ server/estuaryWebSocket.hs
@@ -35,7 +35,9 @@ loop s@(cps,dss) conn = do
   m <- try (WS.receiveData conn)
   case m of
     Right x -> do
-      processResult s (decode (T.unpack x))
+      y <- processResult s (decode (T.unpack x))
+      case y of Just z -> WS.sendTextData conn (T.pack (encodeStrict z))
+                Nothing -> return ()
       loop s conn
     Left WS.ConnectionClosed -> close s "unexpected loss of connection"
     Left (WS.CloseRequest _ _) -> close s "by request from peer"
@@ -46,24 +48,44 @@ close (cps,dss) msg = do
   putStrLn ("connection closed: " ++ msg)
   hush dss
 
-processResult :: TidalState -> Result Request -> IO ()
-processResult _ (Error e) = putStrLn ("Error: " ++ e)
+processResult :: TidalState -> Result Request -> IO (Maybe JSValue)
+processResult _ (Error e) = do
+  putStrLn ("Error: " ++ e)
+  return Nothing
 processResult state (Ok request) = do
   putStrLn (show request)
-  respond state request
+  processRequest state request
 
-respond :: TidalState -> Request -> IO ()
-respond _ (Info i) = return ()
-respond (_,dss) Hush = hush dss
-respond (cps,_) (Cps x) = cps x
-respond (_,dss) (Pattern n p) = do
+processRequest :: TidalState -> Request -> IO (Maybe JSValue)
+
+processRequest _ (Info i) = return Nothing
+
+processRequest (_,dss) Hush = do
+  hush dss
+  return Nothing
+
+processRequest (cps,_) (Cps x) = do
+  cps x
+  return Nothing
+
+processRequest (_,dss) (Pattern n p) = do
   x <- hintParamPattern p
-  case x of (Left error) -> putStrLn "Error interpreting pattern"
-            (Right paramPattern) -> dss!!(n-1) $ paramPattern
-respond _ (Render patt cps cycles) = do
+  case x of (Left error) -> do
+              putStrLn "Error interpreting pattern"
+              return Nothing
+            (Right paramPattern) -> do
+              dss!!(n-1) $ paramPattern
+              return Nothing
+
+processRequest _ (Render patt cps cycles) = do
   x <- hintParamPattern patt
-  case x of (Left error) -> putStrLn "Error interpreting pattern"
-            (Right paramPattern) -> putStrLn (encode (render paramPattern cps cycles))
+  case x of (Left error) -> do
+              putStrLn "Error interpreting pattern"
+              return Nothing
+            (Right paramPattern) -> do
+              let r = render paramPattern cps cycles
+              putStrLn (encodeStrict r)
+              return (Just (showJSON r))
 
 hintParamPattern  :: String -> IO (Either InterpreterError Tidal.ParamPattern)
 hintParamPattern x = Hint.runInterpreter $ do
