4e046d1b1271df82c50ba52fc0f204413cecf5c0
d0kt0r0 <ogbornd@mcmaster.ca>
2016-08-08 19:06:44 -0400

    work in progress on StackedPatterns.hs

--- Estuary/Widgets/StackedPatterns.hs
+++ Estuary/Widgets/StackedPatterns.hs
@@ -2,19 +2,25 @@ module Estuary.Widgets.StackedPatterns where
 
 import Reflex
 import Reflex.Dom
-import Sound.Tidal.Context as Tidal
+import qualified Sound.Tidal.Context as Tidal
 import Estuary.Tidal.Types
+import Estuary.Widgets.Generic
+import Control.Monad
 
 
-stackedPatternsWidget :: Int -> Int -> StackedPatterns -> Event t () -> m (Dynamic t (StackedPatterns,Event t GenericSignal))
+stackedPatternsWidget :: MonadWidget t m => Int -> Int -> StackedPatterns -> Event t () -> m (Dynamic t (StackedPatterns,Event t GenericSignal))
 stackedPatternsWidget rows columns (StackedPatterns iValue) _ = el "table"  $ do
-  rows <- forM [1..rows] $ \r -> el "tr" $ do
-    columns <- forM [1..columns] $ \c -> el "td" $ trivialPatternChain
-  let patterns =  mapDyn fst $ concat rows
-  mapDyn (\a -> (StackedPatterns patterns,never))
+  r <- forM [1..rows] $ \r -> el "tr" $ do
+    c <- forM [1..columns] $ \c -> el "td" $ trivialPatternChain placeHolder never
+    holdDyn 
+  let flat = concat rows -- m [Dynamic t (PatternChain,Event)]
+  firsts <- mapM (mapDyn (fst)) flat -- m [Dynamic t PatternChain]
+  dynList <- collectDyn firsts
+  mapDyn (\x -> (StackedPatterns x,never)) dynList
+  where placeHolder = PatternChain (TransformedPattern [] (S (Atom "bd" Once)))
 
 
-trivialPatternChain :: PatternChain -> Event t () -> m (Dynamic t (PatternChain, Event t GenericSignal))
+trivialPatternChain :: MonadWidget t m => PatternChain -> Event t () -> m (Dynamic t (PatternChain, Event t GenericSignal))
 trivialPatternChain iValue _ = do
   text "trivialPatternChain"
   return $ constDyn (iValue,never)
