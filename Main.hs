33ecaa1accfd017afad48461a5c184728c58625a
Jamie Beverley <jamie_beverley@hotmail.com>
2016-08-09 11:54:11 -0400

    simple s pattern container

--- Main.hs
+++ Main.hs
@@ -6,27 +6,31 @@ import Reflex.Dom
 
 import Estuary.Tidal.Types
 import Estuary.Reflex.Utility
-import Estuary.Widgets.Sound
-import Estuary.Widgets.SoundPattern
+--import Estuary.Widgets.Sound
+--import Estuary.Widgets.SoundPattern
 
 import Control.Monad (liftM)
 import Sound.Tidal.Context
 import Estuary.WebDirt.Stream
 import Estuary.Page
+import Estuary.Widgets.SpecificPattern
 
-import Estuary.Widgets.TransformedPattern
+--import Estuary.Widgets.TransformedPattern
+
+--main = mainWidget $ (simpleSWidget Blank  never)>>= display
 
 main :: IO ()
 main = do
   webDirt <- webDirtStream
   mainWidget $ multipage webDirt pages
 
+pages = [("S-Specific Pattern",widgetToPage $ simpleSWidget (Estuary.Tidal.Types.S Blank) never)]
 
 -- pages :: MonadWidget t m => [m (Dynamic t ParamPattern)]
-pages = [
-  ("trivialSoundPattern",widgetToPage trivialSoundPattern),
-  ("trivialTransformedPattern",widgetToPage trivialTransformedPattern),
-  ("Simple Container",widgetToPage multiTextWidget),
-  ("soundPatternContainer",widgetToPage $ soundPatternContainer emptySoundPattern never),
-  ("Transformed Text Widget",widgetToPage $ transformedTextWidget emptyTransformedPattern never)
-  ]
+-- pages = [
+--   ("trivialSoundPattern",widgetToPage trivialSoundPattern),
+--   ("trivialTransformedPattern",widgetToPage trivialTransformedPattern),
+--   ("Simple Container",widgetToPage multiTextWidget),
+--   ("soundPatternContainer",widgetToPage $ soundPatternContainer emptySoundPattern never),
+--   ("Transformed Text Widget",widgetToPage $ transformedTextWidget emptyTransformedPattern never)
+--   ]
