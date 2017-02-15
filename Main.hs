8ca797479ca6a7cc27ea2f3c3084467e80c0e208
d0kt0r0 <ogbornd@mcmaster.ca>
2017-02-15 16:12:50 -0500

    refactor of top level removing Page.hs and getting ready for WebDirt and other visual feedback above the page

--- Main.hs
+++ Main.hs
@@ -10,10 +10,9 @@ import Estuary.Widgets.StackedPatterns
 import Estuary.Widgets.PatternChain as P
 import Estuary.Widgets.GeneralPattern as G -- for testing the Refactor of general container
 import Control.Monad (liftM)
-import qualified Sound.Tidal.Context as Tidal
+import Sound.Tidal.Context (ParamPattern)
 import Estuary.WebDirt.Foreign
 import Estuary.WebDirt.Stream
-import Estuary.Page
 import Estuary.Widgets.SpecificPattern
 import Estuary.Widgets.WebDirt
 import Data.Map
@@ -25,41 +24,37 @@ main = do
   stream <- webDirtStream wd
   mainWidget $ do
     divClass "estuary" $ do
-      newPage <- divClass "header" $ do
-        divClass "logo" $ text "logo"
-        divClass "webDirt" $ text "webDirt"
-        newPage <- divClass "pageMenu" $ pageMenu
-        divClass "hintArea" $ text "hintArea"
-        return newPage
+      newPage <- header
       divClass "page" $ do
-        let newPage' = fmap (page webDirt) newPage
-        widgetHold (page webDirt (snd (pages!!0))) newPage'
+        let firstPage = snd (pages!!0)
+        let newPage' = fmap (snd . (pages !!)) newPage
+        pattern <- liftM (joinDyn) $ widgetHold firstPage newPage'
+        let patternEval = updated pattern
+        performEvent_ $ fmap (liftIO . stream) patternEval
 
-pageMenu :: (MonadWidget t m) => m (Event t (m (Dynamic t ParamPattern)))
-pageMenu = do
-  let pageNames = Prelude.map (fst) pages
-  let pageList = zipWith (\x y -> (y,x)) pageNames ([0..]::[Int])
-  let pageMap = constDyn $ fromList pageList
-  dd <- dropdown 0 pageMap def
-  return $ fmap (Prelude.map (snd) pages !!) $ _dropdown_change dd
+header :: (MonadWidget t m) => m (Event t Int)
+header = divClass "header" $ do
+  divClass "logo" $ text "logo"
+  divClass "webDirt" $ text "webDirt"
+  newPageIndex <- divClass "pageMenu" $ do
+    let pageNames = Prelude.map (fst) pages
+    let pageList = zipWith (\x y -> (y,x)) pageNames ([0..]::[Int])
+    let pageMap = constDyn $ fromList pageList
+    menu <- dropdown 0 pageMap def
+    return $ _dropdown_change menu
+  divClass "hintArea" $ text "hintArea"
+  return newPageIndex
 
+widgetToPage :: (MonadWidget t m,ParamPatternable p) => m (Dynamic t (p,a)) -> m (Dynamic t ParamPattern)
+widgetToPage x = x >>= mapDyn (toParamPattern . fst)
+
+-- pages :: MonadWidget t m => [(String,m (Dynamic t ParamPattern))]
 pages = [
   ("ICLC Text Widget",widgetToPage $ iclcTextWidget EmptyPatternChain never),
-  ("ICLC Stacked Patterns Widget",widgetToPage twoStackedPatterns),
-  ("ICLC Fixed Widget", widgetToPage $ P.iclcFixedStruct EmptyPatternChain never),
-  ("ICOAH", widgetToPage $ P.icoahWidget EmptyPatternChain never)
+  ("ICLC Stacked Patterns Widget",widgetToPage $ twoStackedPatterns),
+  ("ICLC Fixed Widget",widgetToPage $ P.iclcFixedStruct EmptyPatternChain never),
+  ("ICOAH",widgetToPage $ P.icoahWidget EmptyPatternChain never)
   ]
 
 twoStackedPatterns :: MonadWidget t m => m (Dynamic t (StackedPatterns,Event t GenericSignal))
 twoStackedPatterns = stackedPatternsWidget (StackedPatterns [EmptyPatternChain,EmptyPatternChain]) never
-
-widgetToPage :: (MonadWidget t m, ParamPatternable p) => m (Dynamic t (p,a)) -> m (Dynamic t ParamPattern)
-widgetToPage x = do
-  y <- x
-  mapDyn fst y
-
-page :: (MonadWidget t m) => WebDirtStream -> m (Dynamic t ParamPattern) -> m ()
-page webDirt x = do
-  pattern <- x
-  let evalEvent = updated pattern
-  performEvent_ $ fmap (liftIO . webDirt) evalEvent
--- Makefile
+++ Makefile
@@ -2,7 +2,10 @@ Estuary.jsexe:
 	ghcjs -o Estuary Main.hs
 	cp index.html Estuary.jsexe
 
-static: static
+static: 
 	cp -Rf static/* Estuary.jsexe
 
 all: Estuary.jsexe static
+
+clean:
+	rm -rf Estuary.jsexe
