ec55b3311578935f9a3cc79d6ed1e34789c7311e
d0kt0r0 <ogbornd@mcmaster.ca>
2017-03-01 13:45:53 -0500

    cleanup in Widgets.WebDirt

--- Estuary/Widgets/WebDirt.hs
+++ Estuary/Widgets/WebDirt.hs
@@ -8,20 +8,11 @@ import GHCJS.Types
 import Control.Monad
 import Control.Monad.IO.Class
 
--- webDirtWidget :: MonadWidget t m => JSVal -> m (Dynamic t ())
--- webDirtWidget webDirt = el "div" $ do
---   el "div" $ do
---     text "Esp URL: "
---     t <- textInput def
---     let t' = _textInput_input t
---     performEvent_ $ fmap (liftIO . (syncWithEsp webDirt)) t'
---   return $ constDyn ()
-
 webDirtWidget :: MonadWidget t m => JSVal -> m (Dynamic t ())
 webDirtWidget webDirt = el "div" $ do
   el "div" $ do
-    b <- button "Sync Toggle"
-    syncOn <- toggle False b
-    syncString <- forDyn syncOn (\x-> if x then "ws://intramuros.mcmaster.ca:8002" else "")
-    performEvent_ $ fmap (liftIO . (syncWithEsp webDirt)) $ updated syncString
+    text "Esp URL: "
+    t <- textInput def
+    let t' = _textInput_input t
+    performEvent_ $ fmap (liftIO . (syncWithEsp webDirt)) t'
   return $ constDyn ()
