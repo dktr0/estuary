461a7d2bc9d765118c47a9049d21331e94d7c53f
d0kt0r0 <ogbornd@mcmaster.ca>
2017-05-09 11:05:44 -0400

    work in progress on websocket work around

--- Estuary/Widgets/WebSocket.hs
+++ Estuary/Widgets/WebSocket.hs
@@ -5,13 +5,16 @@ module Estuary.Widgets.WebSocket where
 import Reflex
 import Reflex.Dom
 import Text.JSON
-import Estuary.Protocol
+import Estuary.Protocol.JSON
 import qualified Data.ByteString.Char8 as C
+import Estuary.Protocol.Foreign
 
 
 -- an estuaryWebSocket wraps the underlying Reflex WebSocket with some parsing of the EstuaryProtocol
 -- for collaborative editing. While the password is dynamic, like the Reflex WebSocket the socket address
 -- isn't (so each new address requires a new instance of the widget - see resettingWebSocket below)
+-- currently not working, apparently because of a bug in the old version of reflex-dom we are using
+-- (see alternateWebSocket below)
 
 estuaryWebSocket :: MonadWidget t m => String -> Dynamic t String -> Event t EstuaryProtocol
   -> m (Event t EstuaryProtocol)
@@ -38,14 +41,24 @@ resettingWebSocket addr pwd toSend = do
   ws <- widgetHold (return never) resets
   return $ switchPromptlyDyn ws
 
+alternateWebSocket :: MonadWidget t m => EstuaryProtocolObject -> Event t String -> Dynamic t String - > Event t EstuaryProtocol
+  - m (Event t EstuaryProtocol)
+alternateWebSocket obj addr pwd toSend = do
+  let addr' = fmap ("ws://" ++) addr
+  performEvent_ $ fmap (liftIO . (setUrl obj)) addr'
+  let toSend' = attachDynWith setPassword pwd toSend
+  let toSend'' = fmap (encode) toSend'
+  performEvent_ $ fmap (liftIO . (send obj)) toSend''
+  return never
 
 -- finally, a webSocketWidget includes GUI elements for setting the webSocket address and
 -- password, and connects these GUI elements to a resettingWebSocket (i.e. estuaryWebSocket)
 
-webSocketWidget :: MonadWidget t m => Event t EstuaryProtocol -> m (Event t EstuaryProtocol)
-webSocketWidget toSend = do
+webSocketWidget :: MonadWidget t m => EstuaryProtocolObject -> Event t EstuaryProtocol -> m (Event t EstuaryProtocol)
+webSocketWidget obj toSend = do
   addr <- textInput $ def & textInputConfig_initialValue .~ "127.0.0.1:8002"
   let addr' = tagDyn (_textInput_value addr) (_textInput_keypress addr)
   pwd <- textInput $ def & textInputConfig_initialValue .~ "blah"
   let pwd' = _textInput_value pwd
-  resettingWebSocket addr' pwd' toSend
+  alternateWebSocket obj addr' pwd' toSend
+  -- resettingWebSocket addr' pwd' toSend
