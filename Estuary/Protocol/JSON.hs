443b0eb5583abfd7fa65ce70d51ab15a31751ed5
d0kt0r0 <ogbornd@mcmaster.ca>
2017-07-03 15:51:46 -0400

    began refactor of client-server protocol to support distinct spaces from the same server (as well as other rationalizations)

--- Estuary/Protocol/JSON.hs
+++ Estuary/Protocol/JSON.hs
@@ -1,94 +1,132 @@
 module Estuary.Protocol.JSON where
 
 import Text.JSON
-import Estuary.Tidal.Types
-import Data.Time.Clock.POSIX
 
-data EstuaryProtocol =
-  EstuaryEdit String Int TransformedPattern |
-  TextEdit String Int String |
-  TextEval String Int String |
-  LabelEdit String Int String |
-  Chat String String String | -- password name msg
-  Tempo String Double Double Double | -- password at beat cps
-  TempoChange String Double |
-  ProtocolError String
-  deriving (Show)
 
-instance JSON EstuaryProtocol where
-  showJSON (EstuaryEdit password n code) = encJSDict [("EEdit",showJSON n),("p",showJSON password),("c",showJSON code)]
-  showJSON (TextEdit password n code) = encJSDict [("TEdit",showJSON n),("p",showJSON password),("c",showJSON code)]
-  showJSON (TextEval password n code) = encJSDict [("TEval",showJSON n),("p",showJSON password),("c",showJSON code)]
-  showJSON (LabelEdit password n t) = encJSDict [("LEdit",showJSON n),("p",showJSON password),("t",showJSON t)]
-  showJSON (Chat password name msg) = encJSDict [("Chat",msg),("p",password),("n",name)]
-  showJSON (Tempo password at beat cps) = encJSDict [("Tempo",showJSON cps),("p",showJSON password),("at",showJSON at),("beat",showJSON beat)]
-  showJSON (TempoChange password cps) = encJSDict [("Change",showJSON cps),("p",showJSON password)]
-  showJSON (ProtocolError msg) = encJSDict [("Err",msg)]
-  readJSON (JSObject x) | firstKey x == "EEdit" = EstuaryEdit <$> valFromObj "p" x <*> valFromObj "EEdit" x <*> valFromObj "c" x
-  readJSON (JSObject x) | firstKey x == "TEdit" = TextEdit <$> valFromObj "p" x <*> valFromObj "TEdit" x <*> valFromObj "c" x
-  readJSON (JSObject x) | firstKey x == "TEval" = TextEval <$> valFromObj "p" x <*> valFromObj "TEval" x <*> valFromObj "c" x
-  readJSON (JSObject x) | firstKey x == "LEdit" = LabelEdit <$> valFromObj "p" x <*> valFromObj "LEdit" x <*> valFromObj "t" x
-  readJSON (JSObject x) | firstKey x == "Chat" = Chat <$> valFromObj "p" x <*> valFromObj "n" x <*> valFromObj "Chat" x
-  readJSON (JSObject x) | firstKey x == "Tempo" = Tempo <$> valFromObj "p" x <*> valFromObj "at" x <*> valFromObj "beat" x <*> valFromObj "Tempo" x
-  readJSON (JSObject x) | firstKey x == "Change" = TempoChange <$> valFromObj "p" x <*> valFromObj "Change" x
-  readJSON (JSObject x) | firstKey x == "Err" = ProtocolError <$> valFromObj "Err" x
-  readJSON _ = Error "Unable to parse as EstuaryProtocol"
+data Request a =
+  Authenticate String |
+  RequestSpaceList |
+  JoinSpace String |
+  LeaveSpace |
+  CreateSpace String |
+  SpaceRequest (InSpace (Action a))
 
-setPassword :: String -> EstuaryProtocol -> EstuaryProtocol
-setPassword x (EstuaryEdit _ n c) = EstuaryEdit x n c
-setPassword x (TextEdit _ n c) = TextEdit x n c
-setPassword x (TextEval _ n c) = TextEval x n c
-setPassword x (LabelEdit _ n t) = LabelEdit x n t
-setPassword x (Chat _ m n) = Chat x m n
-setPassword x (Tempo _ a b c) = Tempo x a b c
-setPassword x (TempoChange _ a) = TempoChange x a
+instance JSON a => JSON (Request a) where
+  showJSON (Authenticate p) = encJSDict [("Authenticate",p)]
+  showJSON (RequestSpaceList) = toJSString "RequestSpaceList"
+  showJSON (JoinSpace s) = encJSDict [("JoinSpace",s]
+  showJSON (LeaveSpace) = toJSString "LeaveSpace"
+  showJSON (CreateSpace s) = encJSDict [("CreateSpace",s)]
+  showJSON (SpaceRequest s) = encJSDict [("SpaceRequest",showJSON s)]
+  readJSON (JSObject x) | firstKey x == "Authenticate" = Authenticate <$> valFromObj x "Authenticate"
+  readJSON (JSString x) | fromJSString x == "RequestSpaceList" = Ok RequestSpaceList
+  readJSON (JSObject x) | firstKey x == "JoinSpace" = JoinSpace <$> valFromObj x "JoinSpace"
+  readJSON (JSString x) | fromJSString x == "LeaveSpace" = Ok LeaveSpace
+  readJSON (JSObject x) | firstKey x == "CreateSpace" = CreateSpace <$> valFromObj x "CreateSpace"
+  readJSON (JSObject x) | firstKey x == "SpaceRequest" = SpaceRequest <$> valFromObj x "SpaceRequest"
+  readJSON _ = Error "Unable to parse as Estuary.Protocol.JSON.Request"
 
-matchesNumber :: Int -> EstuaryProtocol -> Bool
-matchesNumber n1 (EstuaryEdit _ n2 _) = n1 == n2
-matchesNumber n1 (TextEdit _ n2 _) = n1 == n2
-matchesNumber n1 (TextEval _ n2 _) = n1 == n2
-matchesNumber n1 (LabelEdit _ n2 _) = n1 == n2
+
+data Response a =
+  SpaceList [String] |
+  SpaceResponse (InSpace (Action a))
+
+instance JSON a => JSON (Response a) where
+  showJSON (SpaceList xs) = encJSDict [("SpaceList",showJSON xs)]
+  showJSON (SpaceResponse r) = encJSDict [("SpaceResponse",showJSON r)]
+  readJSON _ = Error "Unable to parse as Estuary.Protocol.JSON.Request"
+
+
+data InSpace a = InSpace String a
+
+instance JSON a => JSON (InSpace a) where
+  showJSON (InSpace s x) = encJSDict [("InSpace",showJSON s),("x",showJSON x)]
+  readJSON (JSObject x) | firstKey x == "InSpace" = InSpace <$> valFromObj x "InSpace" <$> valFromObj x "x"
+  readJSON _ = Error "Unable to parse as Estuary.Protocol.JSON.InSpace"
+
+
+type Zone = Int
+
+data Action v =
+  Chat String String | -- name message
+  Edit Zone v |
+  Eval Zone v |
+  Tempo Double Double Double | -- at(timepoint) beat(continuous index) cps
+  TempoChange Double
+
+instance JSON v => JSON (Action v) where
+  showJSON (Chat name msg) = encJSDict [("Chat",name),("m",msg)]
+  showJSON (Edit z v) = encJSDict [("Edit",showJSON z),("v",showJSON v)]
+  showJSON (Eval z v) = encJSDict [("Eval",showJSON z),("v",showJSON v)]
+  showJSON (Tempo at beat cps) = encJSDict [("Tempo",showJSON cps),("at",showJSON at),("beat",showJSON beat)]
+  showJSON (TempoChange cps) = encJSDict [("TempoChange",showJSON cps)]
+  readJSON (JSObject x) | firstKey x == "Chat" = Chat <$> valFromObj x "Chat" <*> valFromObj x "m"
+  readJSON (JSObject x) | firstKey x == "Edit" = Edit <$> valFromObj x "Edit" <*> valFromObj x "v"
+  readJSON (JSObject x) | firstKey x == "Eval" = Eval <$> valFromObj x "Eval" <*> valFromObj x "v"
+  readJSON (JSObject x) | firstKey x == "Tempo" = Tempo <$> valFromObj x "Tempo" <*> valFromObj x "at" <*> valFromObj x "beat"
+  readJSON (JSObject x) | firstKey x == "TempoChange" = TempoChange <$> valFromObj x "TempoChange"
+  readJSON _ = Error "Unable to parse as Estuary.Protocol.JSON.Action"
+
+
+matchesNumber :: Int -> Action v -> Bool
+matchesNumber z1 (Edit z2 _) = z1 == z2
+matchesNumber z1 (Eval z2 _) = z1 == z2
 matchesNumber _ _ = False
 
-isEstuaryEdit :: EstuaryProtocol -> Bool
-isEstuaryEdit (EstuaryEdit _ _ _) = True
-isEstuaryEdit _ = False
+matchesZone = matchesNumber
 
-isTextEdit :: EstuaryProtocol -> Bool
-isTextEdit (TextEdit _ _ _) = True
-isTextEdit _ = False
-
-isLabelEdit :: EstuaryProtocol -> Bool
-isLabelEdit (LabelEdit _ _ _) = True
-isLabelEdit _ = False
-
-isChat :: EstuaryProtocol -> Bool
-isChat (Chat _ _ _) = True
+isChat :: Action v -> Bool
+isChat (Chat _ _) = True
 isChat _ = False
 
-justEstuaryCode :: EstuaryProtocol -> TransformedPattern
-justEstuaryCode (EstuaryEdit _ _ x) = x
-justEstuaryCode _ = error "can't get estuary code from non EstuaryEdit"
-
-justTextCode :: EstuaryProtocol -> String
-justTextCode (TextEdit _ _ x) = x
-justTextCode (TextEval _ _ x) = x
-justTextCode _ = error "can't get text code from non TextEdit or TextEval"
-
-justText :: EstuaryProtocol -> String
-justText (TextEdit _ _ x) = x
-justText (TextEval _ _ x) = x
-justText (LabelEdit _ _ x) = x
-justText (Chat _ _ x) = x
-justText _ = error "can't get text from non TextEdit/TextEval/LabelEdit"
-
-isCps :: EstuaryProtocol -> Bool
-isCps (Tempo _ _ _ _) = True
+isCps :: Action v -> Bool
+isCps (Tempo _ _ _) = True
 isCps _ = False
 
-getCps :: EstuaryProtocol -> Maybe Double
-getCps (Tempo _ _ _ x) = Just x
+getCps :: Action v -> Maybe Double
+getCps (Tempo _ _ x) = Just x
 getCps _ = Nothing
+
+
+data ZoneValue =
+  Structure TransformedPattern |
+  EvaluableText String |
+  LabelText String |
+
+instance JSON ZoneValue where
+  showJSON (Structure x) = encJSDict [("Structure",x)]
+  showJSON (EvaluableText x) = encJSDict [("EvaluableText",x)]
+  showJSON (LabelText x) = encJSDict [("LabelText",x)]
+
+isEstuaryEdit :: Action ZoneValue -> Bool
+isEstuaryEdit (Edit _ (Structure _)) = True
+isEstuaryEdit _ = False
+
+isTextEdit :: Action ZoneValue -> Bool
+isTextEdit (Edit _ (EvaluableText _)) = True
+isTextEdit _ = False
+
+isLabelEdit :: Action ZoneValue -> Bool
+isLabelEdit (Edit _ (LabelTexxt _)) = True
+isLabelEdit _ = False
+
+justEstuaryCode :: Action ZoneValue -> TransformedPattern
+justEstuaryCode (Edit _ (Structure x)) = x
+justEstuaryCode _ = error "can't get estuary code from non EstuaryEdit"
+
+justTextCode :: Action ZoneValue -> String
+justTextCode (Edit _ (EvaluableText x)) = x
+justTextCode (Eval _ (EvaluableText x)) = x
+justTextCode _ = error "can't get text code from non TextEdit or TextEval"
+
+justText :: Action ZoneValue -> String
+justText (Edit _ (EvaluableText x)) = x
+justText (Eval _ (EvaluableText x)) = x
+justText (Edit _ (LabelText x)) = x
+justText (Chat _ x) = x
+justText _ = error "can't get text from non TextEdit/TextEval/LabelEdit/Chat"
+
+
 
 -- the function lastOrNothing is useful with fmapMaybe on events containing lists of EstuaryProtocol items
 -- so that no event is fired when the list is empty
@@ -96,4 +134,3 @@ getCps _ = Nothing
 lastOrNothing :: [a] -> Maybe a
 lastOrNothing [] = Nothing
 lastOrNothing xs = Just (last xs)
-
