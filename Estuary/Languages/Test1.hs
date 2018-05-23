0cafb3277850477247ba229ee3d9ccf07857cd55
luis <luisnavarrodelangel@gmail.com>
2018-05-23 13:34:05 -0500

    updates

--- Estuary/Languages/Test1.hs
+++ Estuary/Languages/Test1.hs
@@ -1,7 +1,6 @@
 module Estuary.Languages.Test1 (test1) where
 
 import Text.ParserCombinators.Parsec
-import Data.List (intercalate)
 import Text.ParserCombinators.Parsec.Number
 import qualified Sound.Tidal.Context as Tidal
 --trans
@@ -9,37 +8,35 @@ import qualified Sound.Tidal.Context as Tidal
 
 lengExpr :: GenParser Char a Tidal.ParamPattern
 lengExpr = do
-  espacios
+--coloca aquí los parsers
   s <- sonidos
-  espacios
-  string "trans_"
-  espacios
-  t <- trans
-  espacios
-  return $ t $ nuestroTextoATidal $ s
-  --nuestroTextoATidal $ "~" ++ " "  ++ s1 ++ " " ++ s2 ++ " " ++ "~"
+  t <- transf
+  return $ t $ nuestroTextoATidal s
 
 nuestroTextoATidal ::  String  -> Tidal.ParamPattern
 nuestroTextoATidal s = Tidal.s $ Tidal.p s
 
-espacios :: GenParser Char a String
-espacios = many (oneOf " ")
-
 sonidos :: GenParser Char a String
 sonidos = choice [
-        try (string "bombo" >> espacios >> return "bd"),
-        try (string "maraca" >> espacios >> return "crow")
+        --coloca aqui los nombres de tus muestras de audio
+        --ej. try (string "bombo" >> espacios >> "bd")
+        try (string "~" >> espacios >> "~")
         ]
-
-descartarTexto :: GenParser Char a String
-descartarTexto = many (oneOf "\n")
 
 trans :: GenParser Char a (Tidal.ParamPattern -> Tidal.ParamPattern)
 trans = choice [
-                try (string "fast" >> spaces >> fractional3 False >>= return . Tidal.fast),
-                try (string "density" >> spaces >> fractional3 False >>= return . Tidal.density),
+               --coloca aqui los nombres de tus transformaciones
+              --ej. try (string "fast" >> spaces >> fractional3 False >>= return . Tidal.fast),
                 try (descartarTexto >> return id)
                 ]
+
+--descartar espacios
+espacios :: GenParser Char a String
+espacios = many (oneOf " ")
+
+--descartar texto
+descartarTexto :: GenParser Char a String
+descartarTexto = many (oneOf "\n")
 
 exprStack :: GenParser Char a Tidal.ParamPattern
 exprStack = do
@@ -47,4 +44,4 @@ exprStack = do
    return $ Tidal.stack expr
 
 test1 :: String -> Tidal.ParamPattern
-test1 s = either (const Tidal.silence) id $ parse exprStack "unNombre" s
+test1 s = either (const Tidal.silence) id $ parse exprStack "unNombreparaTuLenguage" s
---
+++ Estuary/Languages/medellin.hs
@@ -0,0 +1,50 @@
+module Estuary.Languages.Medellin (medellin) where
+
+import Text.ParserCombinators.Parsec
+import Data.List (intercalate)
+import Text.ParserCombinators.Parsec.Number
+import qualified Sound.Tidal.Context as Tidal
+--trans
+-- <nombre de sonido> trans_<transformacion>
+
+lengExpr :: GenParser Char a Tidal.ParamPattern
+lengExpr = do
+  espacios
+  s <- sonidos
+  espacios
+  string "trans_"
+  espacios
+  t <- trans
+  espacios
+  return $ t $ nuestroTextoATidal $ s
+  --nuestroTextoATidal $ "~" ++ " "  ++ s1 ++ " " ++ s2 ++ " " ++ "~"
+
+nuestroTextoATidal ::  String  -> Tidal.ParamPattern
+nuestroTextoATidal s = Tidal.s $ Tidal.p s
+
+espacios :: GenParser Char a String
+espacios = many (oneOf " ")
+
+sonidos :: GenParser Char a String
+sonidos = choice [
+        try (string "bombo" >> espacios >> return "bd"),
+        try (string "maraca" >> espacios >> return "crow")
+        ]
+
+descartarTexto :: GenParser Char a String
+descartarTexto = many (oneOf "\n")
+
+trans :: GenParser Char a (Tidal.ParamPattern -> Tidal.ParamPattern)
+trans = choice [
+                try (string "fast" >> spaces >> fractional3 False >>= return . Tidal.fast),
+                try (string "density" >> spaces >> fractional3 False >>= return . Tidal.density),
+                try (descartarTexto >> return id)
+                ]
+
+exprStack :: GenParser Char a Tidal.ParamPattern
+exprStack = do
+   expr <- many lengExpr
+   return $ Tidal.stack expr
+
+medellin :: String -> Tidal.ParamPattern
+medellin s = either (const Tidal.silence) id $ parse exprStack "unNombre" s
