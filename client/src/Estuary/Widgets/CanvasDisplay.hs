6ba4062a61af63281f3668feed8760de5fed2c66
David Ogborn <ogbornd@mcmaster.ca>
2018-11-23 18:11:13 -0500

    holding onto drawing context in rendering of CanvasOps (which solves big performance problem)

--- client/src/Estuary/Widgets/CanvasDisplay.hs
+++ client/src/Estuary/Widgets/CanvasDisplay.hs
@@ -5,6 +5,7 @@ module Estuary.Widgets.CanvasDisplay (canvasDisplay) where
 import Reflex
 import Reflex.Dom
 import GHCJS.DOM.Types (HTMLCanvasElement,castToHTMLCanvasElement)
+import GHCJS.Types (JSVal)
 import Data.JSString
 import Data.Map
 import Control.Monad
@@ -17,31 +18,44 @@ canvasDisplay :: MonadWidget t m => Int -> Dynamic t RenderInfo -> m ()
 canvasDisplay z rInfo = do
   let attrs = fromList [("class","canvasDisplay"),("style","z-index:" ++ show z)]
   e <- liftM (castToHTMLCanvasElement .  _el_element . fst) $ elAttr' "canvas" attrs $ return ()
+  ctx <- liftIO $ getContext e
   instructions <- liftM updated $ mapDyn canvasOps rInfo
-  performEvent_ $ fmap (liftIO . mapM_ (canvasOpIO e)) instructions
+  performEvent_ $ fmap (liftIO . mapM_ (canvasOp ctx)) instructions
 
-canvasOpIO :: HTMLCanvasElement -> CanvasOp -> IO ()
-canvasOpIO e (Rect x y w h) = rect e x y w h >> stroke e
-canvasOpIO e (MoveTo x y) = moveTo e x y
-canvasOpIO e (LineTo x y) = lineTo e x y >> stroke e
-canvasOpIO e (StrokeStyle c) = strokeStyle e (pack $ show c)
-
-foreign import javascript safe
-  "$1.getContext('2d').stroke()"
-  stroke :: HTMLCanvasElement -> IO ()
+canvasOp :: JSVal -> CanvasOp -> IO ()
+canvasOp ctx (Rect x y w h) = beginPath ctx >> rect ctx x y w h >> stroke ctx >> fill ctx
+canvasOp ctx (MoveTo x y) = moveTo ctx x y
+canvasOp ctx (LineTo x y) = beginPath ctx >> lineTo ctx x y >> stroke ctx >> fill ctx
+canvasOp ctx (StrokeStyle c) = strokeStyle ctx (pack $ show c)
 
 foreign import javascript safe
-  "$1.getContext('2d').strokeStyle = $2"
-  strokeStyle :: HTMLCanvasElement -> JSString -> IO ()
+  "$r=$1.getContext('2d')"
+  getContext :: HTMLCanvasElement -> IO JSVal
 
 foreign import javascript safe
-  "$1.getContext('2d').rect($2,$3,$4,$5)"
-  rect :: HTMLCanvasElement -> Double -> Double -> Double -> Double -> IO ()
+  "$1.beginPath()"
+  beginPath :: JSVal -> IO ()
 
 foreign import javascript safe
-  "$1.getContext('2d').moveTo($2,$3)"
-  moveTo :: HTMLCanvasElement -> Double -> Double -> IO ()
+  "$1.stroke()"
+  stroke :: JSVal -> IO ()
 
 foreign import javascript safe
-  "$1.getContext('2d').lineTo($2,$3)"
-  lineTo :: HTMLCanvasElement -> Double -> Double -> IO ()
+  "$1.fill()"
+  fill :: JSVal -> IO ()
+
+foreign import javascript safe
+  "$1.strokeStyle = $2"
+  strokeStyle :: JSVal -> JSString -> IO ()
+
+foreign import javascript safe
+  "$1.rect($2,$3,$4,$5)"
+  rect :: JSVal -> Double -> Double -> Double -> Double -> IO ()
+
+foreign import javascript safe
+  "$1.moveTo($2,$3)"
+  moveTo :: JSVal -> Double -> Double -> IO ()
+
+foreign import javascript safe
+  "$1.lineTo($2,$3)"
+  lineTo :: JSVal -> Double -> Double -> IO ()
