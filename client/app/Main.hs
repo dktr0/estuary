1af38ecfa1c9a13dea960a3857d58cc44bf41615
David Ogborn <ogbornd@mcmaster.ca>
2019-06-03 16:17:52 -0400

    updated to new Punctual version (which uses audioworklets for some things)

--- client/app/Main.hs
+++ client/app/Main.hs
@@ -50,6 +50,9 @@ main = do
 
   pp <- newPeerProtocol
 
+  ac <- getGlobalAudioContext
+  audioWorkletAddModule ac "MusicW-audioWorklets.js"
+
   mainBusNodes@(mainBusIn,_,_,_) <- initializeMainBus
   wd <- liftAudioIO $ newWebDirt mainBusIn
   initializeWebAudio wd
