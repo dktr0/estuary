e98082dd867379c1acf93fd09ba46f4257bd0a63
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Air-2.local>
2019-07-24 10:47:17 -0400

    Vide0 with position fuction and others

--- client/src/Estuary/Languages/Vide0/Parser.hs
+++ client/src/Estuary/Languages/Vide0/Parser.hs
@@ -18,8 +18,10 @@ videoSpec :: ExpParser VideoSpec
 videoSpec =
   literalVideoSpec <|>
   int_VideoSpec <*> int <|>
-  videoSpec_videoSpec <*> videoSpec
+  videoSpec_videoSpec <*> videoSpec -- <|>
+  --rational_rational_videoSpec <*> rational <*> rational
 
+-- Set Video with default options --
 
 literalVideoSpec :: ExpParser VideoSpec
 literalVideoSpec = fmap stringToVideoSpec string
@@ -27,7 +29,7 @@ literalVideoSpec = fmap stringToVideoSpec string
 -- entonces con el fmap estamos usando (a -> b) que es igual a String -> VideoSpec que es igual a stringToVideoSpec, después le damos el f a que es el string y nos va a devolver f b que es ExpParser VideoSpec
 
 --
--- Second option = play video:# (default time)
+-- Set Source Number --
 
 int :: ExpParser Int
 int = fromIntegral <$> integer
@@ -36,16 +38,35 @@ videoSpec_int_videoSpec :: ExpParser (VideoSpec -> Int -> VideoSpec)
 videoSpec_int_videoSpec = setSourceNumber <$ reserved ":"
 
 int_VideoSpec :: ExpParser (Int -> VideoSpec)
-int_VideoSpec =
-  videoSpec_int_videoSpec <*> videoSpec
+int_VideoSpec = videoSpec_int_videoSpec <*> videoSpec
 
 --
--- Third option = change time to the video --
+-- Set Position Cordinates --
 
-int_videoSpec_videoSpec :: ExpParser (Int -> VideoSpec -> VideoSpec)
+rational_rational_videoSpec_videoSpec :: ExpParser (Rational -> Rational -> VideoSpec -> VideoSpec)
+rational_rational_videoSpec_videoSpec = setPosCoord <$ reserved "pos"
+
+--
+-- Time Functions --
+
+--int_videoSpec_videoSpec :: ExpParser (Rational -> VideoSpec -> VideoSpec) --bueno
+int_videoSpec_videoSpec :: ExpParser (Int -> VideoSpec -> VideoSpec) --borrar
 int_videoSpec_videoSpec = playEvery <$ reserved "playEvery"
+
+--int_int_videoSpec_videoSpec :: ExParser (Int -> Int -> VideoSpec -> VideoSpec)
+--int_int_videoSpec_videoSpec = playchop' <$ reserved "playChop'"
+
+--int_int_int_videoSpec_videoSpec :: ExParser (Int -> Int -> Int -> VideoSpec -> VideoSpec)
+--int_int_int_videoSpec_videoSpec = playchop <$ reserved "playChop"
+
+
+--
+-- ExpParser (VideoSpec -> VideoSpec) --
 
 videoSpec_videoSpec :: ExpParser (VideoSpec -> VideoSpec)
 videoSpec_videoSpec =
   playRound <$ reserved "playRound" <|>
-  int_videoSpec_videoSpec <*> int
+  int_videoSpec_videoSpec <*> int <|>
+  rational_rational_videoSpec_videoSpec <*> rational <*> rational
+  --int_int_videoSpec_videoSpec <*> int <*> int <|>
+  --int_int_int_videoSpec_videoSpec <*> int <*> int <*> int
--- client/src/Estuary/Languages/Vide0/VideoSpec.hs
+++ client/src/Estuary/Languages/Vide0/VideoSpec.hs
@@ -11,12 +11,15 @@ import Estuary.Types.Tempo
 data VideoSpec = VideoSpec {
   sampleVideo :: String,
   sourceNumber :: Int,
-  timePosition :: Tempo -> NominalDiffTime -> UTCTime -> NominalDiffTime,
-  timeRate :: Tempo -> NominalDiffTime -> UTCTime -> NominalDiffTime
+  playbackPosition :: Tempo -> NominalDiffTime -> UTCTime -> NominalDiffTime,
+  --playbackRate :: Tempo -> NominalDiffTime -> UTCTime -> Rational, --bueno
+  playbackRate :: Tempo -> NominalDiffTime -> UTCTime -> NominalDiffTime, --borrar
+  posX :: Rational,
+  posY :: Rational
   }
 
 instance Show VideoSpec where
-  show (VideoSpec vs n _ _) = show vs ++ show n
+  show (VideoSpec vs n _ _ px py) = "Sample Video:" ++ show vs ++ " " ++ "Source Number:" ++ show n ++ " " ++ "Position:" ++ show px ++ show py
 
 --instance Show InputsToNetworks where
 -- show (InputsToNetworks rs ps es) = "rolesInputs " ++ (show rs) ++ --"pitchRecognitionInputs " ++ (show ps) ++ "energyInputs " ++ (show es)
@@ -26,24 +29,44 @@ stringToVideoSpec :: String -> VideoSpec
 stringToVideoSpec x = VideoSpec {
   sampleVideo = x,
   sourceNumber = 0,
-  timePosition = VT.playNatural_Pos,
-  timeRate = VT.playNatural_Rate
+  playbackPosition = VT.playNatural_Pos,
+  playbackRate = VT.playNatural_Rate,
+  posX = 0,
+  posY = 0
 }
 
 setSourceNumber :: VideoSpec -> Int -> VideoSpec
 setSourceNumber vs n = vs { sourceNumber = n }
 
+setPosCoord :: Rational -> Rational -> VideoSpec -> VideoSpec
+setPosCoord n m vs = vs { posX = n, posY = m }
+
+--setPosCoord' :: VideoSpec -> Rational -> VideoSpec
+--setPosCoord' vs n = vs { posCoordinates = n }
 
 -- Time Functions --
 
-playEvery :: Int -> VideoSpec -> VideoSpec
+--playEvery :: Rational -> VideoSpec -> VideoSpec --bueno
+playEvery :: Int -> VideoSpec -> VideoSpec --borrar
 playEvery n vs = vs {
-  timePosition = VT.playEvery_Pos n,
-  timeRate = VT.playEvery_Rate n
+  playbackPosition = VT.playEvery_Pos n,
+  playbackRate = VT.playEvery_Rate n
   }
 
 playRound :: VideoSpec -> VideoSpec
 playRound vs = vs {
-  timePosition = VT.playRound_Pos,
-  timeRate = VT.playRound_Rate
+  playbackPosition = VT.playRound_Pos,
+  playbackRate = VT.playRound_Rate
   }
+
+--playChop' :: Int -> Int -> VideoSpec -> VideoSpec
+--playChop' n n vs = vs {
+--  timePosition = VT.something n
+--  timeRate = VT.something n
+--  }
+
+--playChop :: Int -> Int -> Int -> VideoSpec -> VideoSpec
+--playChop n n n vs = vs {
+--  timePosition = VT.something n
+--  timeRate = VT.something n
+--  }
