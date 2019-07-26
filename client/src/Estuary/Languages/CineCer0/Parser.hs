d5f6e08a50e2a978be0d35211fa0fcf0abf7c0db
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Air-2.local>
2019-07-26 17:17:03 -0400

    no specific change

--- client/src/Estuary/Languages/CineCer0/Parser.hs
+++ client/src/Estuary/Languages/CineCer0/Parser.hs
@@ -19,6 +19,10 @@ cineCer0 = f . parseExp
 cineCer0Spec :: ExpParser CineCer0Spec
 cineCer0Spec = fmap (singleton 1) videoSpec
 
+-- have different videos separated with: ;--
+-- add empty action --get rid of te video --
+-- change the rational to integer --
+
 videoSpec :: ExpParser VideoSpec
 videoSpec =
   literalVideoSpec <|>
