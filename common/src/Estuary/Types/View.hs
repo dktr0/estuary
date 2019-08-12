cbf0eb92827e18465df5c2d13ead4e052255f808
David Ogborn <ogbornd@mcmaster.ca>
2019-08-12 17:26:50 -0400

    sketch of new direction for View system added to code without substantial integration/implementation

--- common/src/Estuary/Types/View.hs
+++ common/src/Estuary/Types/View.hs
@@ -8,7 +8,9 @@ import Data.Aeson
 
 data View =
   Views [View] |
-  ViewDiv Text View |
+  ViewDiv Text View | -- deprecated in favour of next two...
+  Row Rational View | -- a full row in the layout, number is percent of vertical space
+  Cell Rational View | -- a cell (ie. column fragment) in the layout, number is percent of horizontal space
   LabelView Int |
   StructureView Int |
   TextView Int Int | -- first int is zone to edit, second int is number of lines in editor
@@ -21,3 +23,13 @@ instance FromJSON View
 
 emptyView :: View
 emptyView = Views []
+
+viewToRows :: View -> Int
+viewToRows (Views vs) = sum $ fmap viewToRows vs
+viewToRows (ViewDiv _ v) = viewToRows v
+viewToRows (RowView _ v) = viewToRows v
+viewToRows (CellView _ v) = viewToRows v
+viewToRows (LabelView _) = 1
+viewToRows (StructureView _) = 1 -- ???
+viewToRows (SequenceView _) = 1 -- ???
+viewToRows (TextView _ n) = n + 1
