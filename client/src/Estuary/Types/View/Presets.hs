b35004a1fea01846c832eb3024101ece39b3481b
luis <luisnavarrodelangel@gmail.com>
2019-09-20 06:38:27 -0400

    5 rows

--- client/src/Estuary/Types/View/Presets.hs
+++ client/src/Estuary/Types/View/Presets.hs
@@ -90,11 +90,11 @@ presetViews = fromList [
      ]),
 
   ("memorias",Views [
-     ViewDiv "eightMiddleL" (Views [LabelView 0,TextView 1 10]),
-     ViewDiv "eightMiddleR" (Views [LabelView 2,TextView 3 10]),
-     ViewDiv "eightMiddleL" (Views [LabelView 4,TextView 5 10]),
-     ViewDiv "eightMiddleR" (Views [LabelView 6,TextView 7 10]),
-     ViewDiv "eightMiddleL" (Views [LabelView 8,TextView 9 10]),
-     ViewDiv "eightMiddleR" (Views [LabelView 10,TextView 11 10])
+     ViewDiv "eightMiddleL" (Views [LabelView 0,TextView 1 5]),
+     ViewDiv "eightMiddleR" (Views [LabelView 2,TextView 3 5]),
+     ViewDiv "eightMiddleL" (Views [LabelView 4,TextView 5 5]),
+     ViewDiv "eightMiddleR" (Views [LabelView 6,TextView 7 5]),
+     ViewDiv "eightMiddleL" (Views [LabelView 8,TextView 9 5]),
+     ViewDiv "eightMiddleR" (Views [LabelView 10,TextView 11 5])
      ])
   ]
