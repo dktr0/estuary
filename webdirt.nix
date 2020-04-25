797b7a59b4bb95d5904406cbe519d97bca2a5274
d0kt0r0 <ogbornd@mcmaster.ca>
2020-04-25 12:05:51 -0400

    bump to WebDirt with alphabetical sorting in makeSampleMap.sh

--- webdirt.nix
+++ webdirt.nix
@@ -11,7 +11,7 @@ in
     '';
     src = builtins.fetchGit {
       url = "https://github.com/dktr0/WebDirt.git";
-      rev = "8e179fd8d12bfe85be6392d2300aea726418295d";
+      rev = "ea779f5a5ec2a13ba2d8dee9f85a710132cd9331";
       ref = "master";
     };
   }
