72f6f88c8f1be6c9eebb1e4a4760fdb7d7f42358
d0kt0r0 <ogbornd@mcmaster.ca>
2020-04-27 18:27:50 -0400

    bump to WebDirt version with reworked bandpass lowpass and highpass filters

--- webdirt.nix
+++ webdirt.nix
@@ -11,7 +11,7 @@ in
     '';
     src = builtins.fetchGit {
       url = "https://github.com/dktr0/WebDirt.git";
-      rev = "fca6567e88d46a04bef30defa63092a3fd7d28ce";
+      rev = "9641db475d8113fd6e1c28612fe51b05f069fab6";
       ref = "master";
     };
   }
