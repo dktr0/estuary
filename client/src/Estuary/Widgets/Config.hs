e27d2ced6816405f6304f0908ee73e70cea3a00e
Alex MacLean <maclean199@gmail.com>
2020-06-17 14:57:09 -0400

    Config option descriptions in divs

--- client/src/Estuary/Widgets/Config.hs
+++ client/src/Estuary/Widgets/Config.hs
@@ -20,52 +20,69 @@ configWidget :: MonadWidget t m => Editor t m (Event t ContextChange)
 configWidget = do
 
   canvasEnabledEv <- divClass "config-option primary-color ui-font" $ do
-    dynText =<< (translatableText $ fromList [
-      (English,"Placeholder text for canvas option "),
-      (Español,"Spanish placeholder text for canvas option" )
-      ])
-    dynText =<< (translatableText $ fromList [
-      (English,"Canvas: "),
-      (Español,"Canvas: ")
-      ])
+    text "Canvas: "
     canvasInput <- elClass "label" "switch" $ do
       x <- checkbox True def
       elClass "span" "slider round" (return x)
+    el "div" $ dynText =<< (translatableText $ fromList [
+      (English,"Placeholder text for canvas option "),
+      (Español,"Spanish placeholder text for canvas option" )
+      ])
     return $ fmap (\x -> \c -> c { canvasOn = x }) $ _checkbox_change canvasInput
 
   superDirtEnabledEv <- divClass "config-option primary-color ui-font" $ do
-    text "SuperDirt:"
+    text "SuperDirt: "
     sdInput <- elClass "label" "switch" $ do
       x <- checkbox False def
       elClass "span" "slider round" (return x)
+    el "div" $ dynText =<< (translatableText $ fromList [
+      (English,"Placeholder text for superdirt option "),
+      (Español,"Spanish placeholder text for superdirt option" )
+      ])
     return $ fmap (\x -> (\c -> c { superDirtOn = x } )) $ _checkbox_change sdInput
 
   webDirtEnabledEv <- divClass "config-option primary-color ui-font" $ do
-    text "WebDirt:"
+    text "WebDirt: "
     wdInput <-elClass "label" "switch" $ do
       x <- checkbox True def
       elClass "span" "slider round" (return x)
+    el "div" $ dynText =<< (translatableText $ fromList [
+      (English,"Placeholder text for webdirt option "),
+      (Español,"Spanish placeholder text for webdirt option" )
+      ])
     return $ fmap (\x -> (\c -> c { webDirtOn = x } )) $ _checkbox_change wdInput
 
   dynamicsModeEv <- divClass "config-option primary-color ui-font" $ do
-    text "Dynamics:"
+    text "Dynamics: "
     let dmMap = fromList $ zip dynamicsModes (fmap (T.pack . show) dynamicsModes)
     dmChange <- _dropdown_change <$> dropdown DefaultDynamics (constDyn dmMap) (def & attributes .~ constDyn ("class" =: "ui-dropdownMenus primary-color primary-borders ui-font" <> "style" =: "background-color: transparent"))
+    el "div" $ dynText =<< (translatableText $ fromList [
+      (English,"Placeholder text for dynamics option "),
+      (Español,"Spanish placeholder text for dynamics option" )
+      ])
     return $ fmap (\x c -> c { dynamicsMode = x }) dmChange -- context -> context
 
   resolutionChangeEv <- divClass "config-option primary-color ui-font" $ do
     term Term.Resolution >>= dynText
-    text ":"
+    text ": "
     let resolutions = [Punctual.QHD,Punctual.FHD,Punctual.HD,Punctual.WSVGA,Punctual.SVGA,Punctual.VGA,Punctual.QVGA]
     let resMap = fromList $ zip resolutions $ fmap (T.pack . show) resolutions
     resChange <- _dropdown_change <$> dropdown Punctual.HD (constDyn resMap) (def & attributes .~ constDyn ("class" =: "ui-dropdownMenus primary-color primary-borders ui-font"))
+    el "div" $ dynText =<< (translatableText $ fromList [
+      (English,"Placeholder text for resolution option "),
+      (Español,"Spanish placeholder text for resolution option" )
+      ])
     return $ fmap (\x c -> c { resolution = x }) resChange
 
   brightnessChangeEv <- divClass "config-option primary-color ui-font" $ do
     term Term.Brightness >>= dynText
-    text ":"
+    text ": "
     let brightnessMap = fromList [(1.0,"100%"),(0.5,"50%"),(0.25,"25%"),(0.1,"10%")]
     brightnessChange <- _dropdown_change <$> dropdown 1.0 (constDyn brightnessMap) (def & attributes .~ constDyn ("class" =: "ui-dropdownMenus primary-color primary-borders ui-font"))
+    el "div" $ dynText =<< (translatableText $ fromList [
+      (English,"Placeholder text for brightness option "),
+      (Español,"Spanish placeholder text for brightness option" )
+      ])
     return $ fmap (\x c -> c { brightness = x }) brightnessChange
 
   return $ mergeWith (.) [canvasEnabledEv, superDirtEnabledEv, webDirtEnabledEv, dynamicsModeEv, resolutionChangeEv, brightnessChangeEv]
