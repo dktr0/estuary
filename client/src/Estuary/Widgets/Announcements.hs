aef7de8babbf93386035e32c4ef24a38d3b2eeee
d0kt0r0 <ogbornd@mcmaster.ca>
2020-06-23 12:11:16 -0400

    made some initial compiled-in announcements

--- client/src/Estuary/Widgets/Announcements.hs
+++ client/src/Estuary/Widgets/Announcements.hs
@@ -14,14 +14,6 @@ import Estuary.Types.TranslatableText
 import Estuary.Reflex.Utility
 import Estuary.Widgets.Editor
 
-
-splitPageWithAnnouncements :: MonadWidget t m => Editor t m a -> Editor t m a
-splitPageWithAnnouncements child = divClass "pageSplitter" $ do
-  r <- divClass "halfPage" $ child
-  divClass "halfPage" announcementsWidget
-  return r
-
-
 -- compiledAnnouncements contains "compiled-in" news and announcements, which are part
 -- of the client and call attention to new features, changes, etc. These are
 -- merged with dynamic announcements from the server to form the total displayed
@@ -29,13 +21,27 @@ splitPageWithAnnouncements child = divClass "pageSplitter" $ do
 
 compiledAnnouncements :: Map Day [TranslatableText]
 compiledAnnouncements = fromList [
-  (read "2020-06-22",[ fromList [
-    (English,"This is a compiled announcement.")
+  (read "2020-06-19",[ fromList [
+    (English,"Configuration settings like turning Canvas on/off, selecting audio dynamics presets, etc are now accessed by clicking the ? button on the top right, then clicking on the settings tab. The settings can be hidden again by clicking on the ? again.")
     ]]),
-  (read "2020-05-10",[ fromList [
-    (English,"This is another compiled announcement.")
+  (read "2020-06-16",[ fromList [
+    (English,"Estuary now uses version 1.6.0 of Tidal to provide MiniTidal functionality.")
+    ]]),
+  (read "2020-06-07",[ fromList [
+    (English,"Estuary now includes the latest version of Punctual, with support for non-band-limited oscillators (lfsqr,lftri,lfsaw) and the 'step' function for sequencing.")
+    ]]),
+  (read "2020-06-01",[ fromList [
+    (English,"Estuary now supports tempo changes from the terminal. Use !setcps or !setbpm followed by a number to immediately change the tempo. !setcps sets the tempo in cycles/bars per seconds. !setbpm sets the tempo in beats per minute (based on the model of 4 presumed beats per cycle/bar).")
     ]])
   ]
+
+
+splitPageWithAnnouncements :: MonadWidget t m => Editor t m a -> Editor t m a
+splitPageWithAnnouncements child = divClass "pageSplitter" $ do
+  r <- divClass "halfPage" $ child
+  divClass "halfPage" announcementsWidget
+  return r
+
 
 announcementsWidget :: MonadWidget t m => Editor t m ()
 announcementsWidget = divClass "announcements" $ do
@@ -55,7 +61,7 @@ announcementsWidget = divClass "announcements" $ do
     ctx <- context
     serverAnnouncements <- holdUniqDyn $ fmap announcements ctx
     let annMap = fmap (unionWith (++) compiledAnnouncements) serverAnnouncements
-    let xs = fmap (concat . mapWithKey  (\k a -> zip (repeat k) a)) annMap
+    let xs = fmap (reverse . concat . mapWithKey  (\k a -> zip (repeat k) a)) annMap
     simpleList xs individualAnnouncement
 
   return ()
@@ -63,5 +69,5 @@ announcementsWidget = divClass "announcements" $ do
 individualAnnouncement :: MonadWidget t m => Dynamic t (Day,TranslatableText) -> Editor t m ()
 individualAnnouncement x = divClass "announcement" $ do
   dynText $ fmap (T.pack . show . fst) x
-  text "+ "
+  text ": "
   dynText =<< dynTranslatableText (fmap snd x)
