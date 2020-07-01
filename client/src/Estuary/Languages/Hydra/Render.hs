faec48b1bdb9646b7b1dd47345d75e2a4e56e87f
d0kt0r0 <ogbornd@mcmaster.ca>
2020-06-30 20:11:11 -0400

    hydra render reworked around non-global Hydra functions

--- client/src/Estuary/Languages/Hydra/Render.hs
+++ client/src/Estuary/Languages/Hydra/Render.hs
@@ -15,50 +15,53 @@ instance PToJSVal Hydra where pToJSVal (Hydra x) = x
 instance PFromJSVal Hydra where pFromJSVal = Hydra
 
 foreign import javascript safe
-  "new Hydra({ canvas:$1, autoLoop: false})"
+  "new Hydra({ canvas:$1, autoLoop:false, makeGlobal:false})"
   newHydra :: HTMLCanvasElement -> IO Hydra
 
 foreign import javascript safe
   "$1.setResolution($2,$3);"
   setResolution :: Hydra -> Int -> Int -> IO ()
 
-evaluate :: [Statement] -> IO ()
-evaluate = mapM_ evaluateStatement
+evaluate :: Hydra -> [Statement] -> IO ()
+evaluate h = mapM_ (evaluateStatement h)
 
 foreign import javascript safe
   "$1.tick($2);"
   tick :: Hydra -> Double -> IO () -- second argument is time in millis since last tick
 
-instance PToJSVal Source where
-  pToJSVal (ConstantInt x) = pToJSVal x
-  pToJSVal (ConstantDouble x) = pToJSVal x
-  -- pToJSVal (List xs) = pToJSVal xs
-  pToJSVal (Fast x) = _fast (pToJSVal x)
-  pToJSVal (Osc x y z) = _osc (pToJSVal x) (pToJSVal y) (pToJSVal z)
-  pToJSVal _ = nullRef -- placeholder: should be removed once all patterns are matched
+sourceToJSVal :: Hydra -> Source -> JSVal
+sourceToJSVal _ (ConstantInt x) = pToJSVal x
+sourceToJSVal _ (ConstantDouble x) = pToJSVal x
+-- sourceToJSVal _ (List xs) = pToJSVal xs
+sourceToJSVal h (Fast x) = _fast h (maybeSourceToJSVal h x)
+sourceToJSVal h (Osc x y z) = _osc h (maybeSourceToJSVal h x) (maybeSourceToJSVal h y) (maybeSourceToJSVal h z)
 
-foreign import javascript safe "fast($1)" _fast :: JSVal -> JSVal
-foreign import javascript safe "osc($1,$2,$3)" _osc :: JSVal -> JSVal -> JSVal -> JSVal
+foreign import javascript safe "$1.synth.fast($2)" _fast :: Hydra -> JSVal -> JSVal
+foreign import javascript safe "$1.synth.osc($2,$3,$4)" _osc :: Hydra -> JSVal -> JSVal -> JSVal -> JSVal
 
-instance PToJSVal Output where
-  pToJSVal O0 = _o0
-  pToJSVal O1 = _o1
-  pToJSVal O2 = _o2
-  pToJSVal O3 = _o3
+maybeSourceToJSVal :: Hydra -> Maybe Source -> JSVal
+maybeSourceToJSVal _ Nothing = nullRef
+maybeSourceToJSVal h (Just x) = sourceToJSVal h x
 
-foreign import javascript safe "o0" _o0 :: JSVal
-foreign import javascript safe "o1" _o1 :: JSVal
-foreign import javascript safe "o2" _o2 :: JSVal
-foreign import javascript safe "o3" _o3 :: JSVal
+outputToJSVal :: Hydra -> Output -> JSVal
+outputToJSVal h O0 = _o0 h
+outputToJSVal h O1 = _o1 h
+outputToJSVal h O2 = _o2 h
+outputToJSVal h O3 = _o3 h
 
-evaluateStatement :: Statement -> IO ()
-evaluateStatement (Out s o) = _out (pToJSVal s) (pToJSVal o)
-evaluateStatement (Render o) = _render (pToJSVal o)
+foreign import javascript safe "$1.synth.o0" _o0 :: Hydra -> JSVal
+foreign import javascript safe "$1.synth.o1" _o1 :: Hydra -> JSVal
+foreign import javascript safe "$1.synth.o2" _o2 :: Hydra -> JSVal
+foreign import javascript safe "$1.synth.o3" _o3 :: Hydra -> JSVal
+
+evaluateStatement :: Hydra -> Statement -> IO ()
+evaluateStatement h (Out s o) = _out (sourceToJSVal h s) (outputToJSVal h o)
+evaluateStatement h (Render o) = _render h (outputToJSVal h o)
 
 foreign import javascript safe
   "$1.out($2);"
   _out :: JSVal -> JSVal -> IO ()
 
 foreign import javascript safe
-  "render($1);"
-  _render :: JSVal -> IO ()
+  "$1.synth.render($2);"
+  _render :: Hydra -> JSVal -> IO ()
