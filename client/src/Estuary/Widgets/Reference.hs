f7144465081bd84e684ca36b4cbd4fe7a8a607e8
Alex MacLean <maclean199@gmail.com>
2020-07-03 17:30:15 -0400

    Start adding items to main reference page

--- client/src/Estuary/Widgets/Reference.hs
+++ client/src/Estuary/Widgets/Reference.hs
@@ -6,4 +6,15 @@ import Reflex hiding (Request,Response)
 import Reflex.Dom hiding (Request,Response)
 
 referenceWidget :: MonadWidget t m => m ()
-referenceWidget = return ()
+referenceWidget = divClass "reference" $ do
+  divClass "reference-title" $ text "estuary"
+  divClass "reference-description" $ text "Estuary is a platform for collaboration and learning through live coding. It enables you to create sound, music, and visuals in a web browser."
+  el "h3" $ text "Languages"
+  el "ul" $ do
+    el "li" (text "MiniTidal")
+    el "li" (text "Punctual")
+    el "li" (text "CineCer0")
+  el "h3" $ text "Using the Terminal"
+  el "h3" $ text "Keyboard Shortcuts"
+  el "h3" $ text "Tips and Tricks"
+  return ()
