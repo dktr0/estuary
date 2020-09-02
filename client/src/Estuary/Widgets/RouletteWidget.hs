71edfd6885efd0218229a57bc181b2e63343a81d
d0kt0r0 <ogbornd@mcmaster.ca>
2020-09-02 14:51:54 -0400

    bug fix to roulette widget

--- client/src/Estuary/Widgets/RouletteWidget.hs
+++ client/src/Estuary/Widgets/RouletteWidget.hs
@@ -28,44 +28,35 @@ import qualified Estuary.Types.Term as Term
 
 rouletteWidget :: MonadWidget t m => Dynamic t Roulette -> Editor t m (Variable t Roulette)
 rouletteWidget delta = divClass "rouletteContainer" $ mdo
+
     ctx <- context
-    let ensC = fmap ensembleC ctx
-    let uHandle = fmap userHandle ensC -- Dynamic Text
-    uHandle' <- sample $ current uHandle -- Text
+    initialHandle <- sample $ current $ fmap (userHandle . ensembleC) ctx
 
-    let lineUpBool' = fmap (lineUpBool uHandle') currentVal
-    let dynAttrs = attrsLineUpButton <$> lineUpBool'
+    let attrsRouletteButton' = attrsRouletteButton initialHandle
+    listOfRouletteButtons <- simpleList currentVal (rouletteButton attrsRouletteButton')
+    let deleteEv = switchDyn $ fmap leftmost listOfRouletteButtons
 
-    let listOfRouletteButtons' = fmap leftmost listOfRouletteButtons -- Dynamic t (Event t (Roulette -> Roulette))
-    let listOfRouletteButtons'' = switchDyn listOfRouletteButtons'  -- m (Event t (Roulette -> Roulette))
-    let attrsRouletteButton' = attrsRouletteButton uHandle'  --  Map Text Text
-    listOfRouletteButtons <- simpleList currentVal (rouletteButton attrsRouletteButton') -- m (Dynamic [Event t (Roulette -> Roulette)])
+    let dynAttrs = attrsLineUpButton <$> fmap (currentlyInLine initialHandle) currentVal
+    lineupEv <- lineUpButton dynAttrs "+" (addHandleToList initialHandle)
 
-    lineUpButton' <- lineUpButton dynAttrs "+" (addHandleToList uHandle') -- (Event t (Roulette -> Roulette)
-
-    currentVal <- holdUniqDyn $ currentValue x -- Dynamic t [Text]
-    let currentValToBehaviour = current (currentValue x) -- Behaviour RouletteView
-    let buttonEvs = mergeWith (.) [listOfRouletteButtons'', lineUpButton'] -- Event t (Roulette -> Roulette)
-    let newValue = attachWith (flip ($)) currentValToBehaviour buttonEvs  -- Event t Roulette
+    let editEvs = mergeWith (.) [deleteEv,lineupEv]
+    let newValue = traceEvent "newValue" $ attachWith (flip ($)) (current currentVal) editEvs
     x <- returnVariable delta newValue
+    currentVal <- holdUniqDyn $ currentValue x
     return x
 
--- Dynamic t a (comes from the server, and only is the initial value and it also represents the udpates) -> Event t a (come from local user actions) -> Variable t Roulette
-lineUpBool :: Text -> [Text] -> Bool
-lineUpBool uHandle roulette
+currentlyInLine :: Text -> [Text] -> Bool
+currentlyInLine uHandle roulette
   |elem uHandle roulette = True
   |uHandle == "" = True
   |otherwise = False
 
 rouletteButton :: MonadWidget t m =>  Map Text Text -> Dynamic t Text -> m (Event t (Roulette -> Roulette))
 rouletteButton attrs label = do
-  label' <- sample $ current label-- Text
-  let r = removeHandleFromList $ label' -- <> "     " <> "x"  -- Dynamic t (Roulette -> Roulette)
   (element, _) <- elAttr' "div" attrs $ dynText $ label <> (constDyn " ") <> (constDyn "ⓧ")
   clickEv <- wrapDomEvent (_el_element element) (elementOnEventName Click) (mouseXY)
-  let roulette = r <$ clickEv
-  return roulette
-
+  return $ attachWith (\x _ -> removeHandleFromList x) (current label) clickEv
+  
 attrsRouletteButton :: Text -> Map Text Text
 attrsRouletteButton uhandle
   | uhandle == "" = "class" =: "rouletteButtons ui-buttons code-font" <> "style" =: "cursor: not-allowed; pointer-events: none;"
@@ -88,20 +79,13 @@ attrsLineUpButton b = "class" =: "lineUpButton ui-buttons other-borders code-fon
     colour False =  "color:var(--primary-color)"
     colour True =  "color: var(--secondary-color)"
 
-
 rouletteToRoulette :: Dynamic t Roulette -> Dynamic t Roulette
 rouletteToRoulette xs = xs
-
 
 addHandleToList :: Text -> Roulette -> Roulette
 addHandleToList uHandle roulette
   |elem uHandle roulette = roulette
   |otherwise = (++) roulette [uHandle]
---addHandleToList "luis" -- Roulette -> Roulette -- check if the name is in the list already and add if not.
--- Both of these func should be pure funcs.
-
--- e.g. deleteHandleFromL :: Text -> Roulette -> Roulette
--- deleteHandleFromL -- fail silently , i.e. return the existing list
 
 removeHandleFromList :: Text -> Roulette -> Roulette
 removeHandleFromList handle xs = Prelude.filter (\e -> e/=handle) xs
