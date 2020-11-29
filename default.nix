0c18a2e26158cc469b7e6260a7645d9daf464553
luis <luisnavarrodelangel@gmail.com>
2020-11-29 15:44:00 -0500

    About translation updated and Seis8s new version

--- default.nix
+++ default.nix
@@ -224,8 +224,8 @@ in
           dontHaddock (self.callCabal2nix "seis8s" (pkgs.fetchFromGitHub {
            owner = "luisnavarrodelangel";
            repo = "seis8s";
-           sha256 = "15clxgpw0ls8fbpc451045c7kqgkqgilfah7vxygaa5c05k31ivm";
-           rev = "c96aef32bf9dc0e03685c38f8d8c50cda9612157";
+           sha256 = "0fyiiqya4rfq9h8q17rarv06vizd967jxc353kznadj93asi4his";
+           rev = "a3f9ea35b9037df3f8405dc66c20b19f92b1a675";
          }) {});
       };
     in
