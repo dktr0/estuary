4a641845ee53530bf8e68299b87c36b39593a42b
Alejandro <afrancob@nil1.mcmaster.ca>
2021-09-09 18:02:01 -0400

    added a metric visualiser rather than cyclicgit add . learned to use simpleList, etc

--- client/src/Estuary/Widgets/Tempo.hs
+++ client/src/Estuary/Widgets/Tempo.hs
@@ -51,7 +51,7 @@ visualiseTempoWidget delta = divClass "tempoVisualiser" $  mdo
 
   dynTempo <- holdDyn 0 elapsedBeatsRunning
 
-  out <- visualiseCycles dynTempo
+  out <- visualiseMetre dynTempo
 
   v <- variable delta never
   return v
@@ -61,6 +61,8 @@ getElapsedBeats t now = do
   let x = timeToCount t now 
   return x
 
+
+---- separate the view Box from the circle, so this function can be a generic container for the metric and cyclic vis
 visualiseCycles :: MonadWidget t m => Dynamic t Rational -> W t m ()
 visualiseCycles delta = do
   let class' = constDyn $ "class" =: "human-to-human-comm code-font"
@@ -73,7 +75,7 @@ visualiseCycles delta = do
   let (cx,cy) = (constDyn $ "cx" =: "0", constDyn $ "cy" =: "0")
   let r = constDyn $ "r" =: "1"
   let stroke = constDyn $ "stroke" =: "white"
-  let strokeWidth = constDyn $ "stroke-width" =: "0.05"
+  let strokeWidth = constDyn $ "stroke-width" =: "0.05; filter: blur(%50)" -- little experiment with blur
   let attrsCircle = mconcat [cx,cy,r,stroke,strokeWidth]
 
   let (x1,x2) = (constDyn $ "x1" =: "0",constDyn $ "x2" =: "0")
@@ -99,6 +101,10 @@ beatToRotation r = "transform" =: ("rotate(" <> (showt radio) <> ")")
   where radio = fromIntegral (round $ radio' * 360) :: Double
         radio' = r - (realToFrac $ floor r)
  
+beatToPercentage:: Text -> Rational -> Map Text Text
+beatToPercentage atr beat = atr =: (showt percen)
+  where percen = fromIntegral (round $ percen' * 100) :: Double
+        percen' = beat - (realToFrac $ floor beat)
 
 visualiseMetre :: MonadWidget t m => Dynamic t Rational -> W t m ()
 visualiseMetre delta = do
@@ -107,30 +113,50 @@ visualiseMetre delta = do
   let vB = constDyn $ "viewBox" =: "0 0 100 100"
   let w' = constDyn $ "width" =: "100"
   let h' = constDyn $ "height" =: "100"
+  let stroke = constDyn $ "stroke" =: "white"
+  let strokeWidth = constDyn $ "stroke-width" =: "0.05;" 
+
   let attrs = mconcat [class',w',h',style,vB]
 
-  let (x,y) = (constDyn $ "x" =: "0", constDyn $ "y" =: "40")
-  let (w,h) = (constDyn $ "width" =: "100", constDyn $ "height" =: "30",)
-  let stroke = constDyn $ "stroke" =: "white"
-  let strokeWidth = constDyn $ "stroke-width" =: "0.05"
-  let attrsRect = mconcat [cx,cy,r,stroke,strokeWidth]
+  let x1 = beatToPercentage "x1" <$> delta
+  let x2 = beatToPercentage "x2" <$> delta
+  let (y1,y2) = (constDyn $ "y1" =: "0",constDyn $ "y2" =: "100")
+  let attrsLine = mconcat [x1,y1,x2,y2,stroke,strokeWidth]
 
-  let (x1,x2) = (constDyn $ "x1" =: "0",constDyn $ "x2" =: "0")
-  let (y1,y2) = (constDyn $ "y1" =: "0",constDyn $ "y2" =: "-1")
-  let transform = beatToRotation <$> delta 
-  let attrsLine = mconcat [x1,y1,x2,y2,stroke,strokeWidth,transform]
-
-  let attrsLine' = mconcat [x1,y1,x2,y2,stroke,strokeWidth]
 
  -- elDynAttr "stopwatch" attrs $ dynText $ fmap (showt) $ fmap (showt) delta
   elDynAttrNS' (Just "http://www.w3.org/2000/svg") "svg" attrs $ do
-    -- create circular dial
-    elDynAttrNS' (Just "http://www.w3.org/2000/svg") "circle" attrsRect $ return () 
     -- manecilla
     elDynAttrNS' (Just "http://www.w3.org/2000/svg") "line" attrsLine $ return ()
     -- mark
-    elDynAttrNS' (Just "http://www.w3.org/2000/svg") "line" attrsSegs $ return () 
+    generateSegments 100 4
   return ()
+
+
+-- simpleList :: MonadWidget t m => Dynamic t [v] -> (Dynamic t v -> m a) -> m (Dynamic t [a]) 
+
+generateSegments:: MonadWidget t m => Rational -> Rational ->  m ()
+generateSegments width nLines = do
+  let segmentsSize = width / nLines 
+      lineList = constDyn $ Prelude.take (floor nLines) $ iterate (+ segmentsSize) 0
+  x <- simpleList lineList (generateSegment)
+  return ()
+  
+generateSegment:: MonadWidget t m => Dynamic t Rational ->  m ()
+generateSegment x = do
+  let x1 = generateAttr "x1" <$> x
+  let x2 = generateAttr "x2" <$> x
+  let y1 = constDyn $ "y1" =: "0"
+  let y2 = constDyn $ "y2" =: "100"
+  let stroke = constDyn $ "stroke" =: "white"
+  let strokeWidth = constDyn $ "stroke-width" =: "0.05;"
+  let attrsLine = mconcat [x1,y1,x2,y2,stroke,strokeWidth]
+  elDynAttrNS' (Just "http://www.w3.org/2000/svg") "line" attrsLine $ return ()
+  return ()
+  
+generateAttr :: Text -> Rational -> Map Text Text
+generateAttr atr x = atr =: (showt (realToFrac x :: Double))
+
 
 -- <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
 --   <defs>
