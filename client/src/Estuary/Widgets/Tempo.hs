586fcc7551f201bca47bf19d138ef96a750ea95d
Alejandro <afrancob@nil1.mcmaster.ca>
2021-09-15 19:56:10 -0400

    added a raw and not very functional ring representation, needs work...

--- client/src/Estuary/Widgets/Tempo.hs
+++ client/src/Estuary/Widgets/Tempo.hs
@@ -41,7 +41,7 @@ tempoWidget tempoDyn = do
   return $ localEdits v
 
 
-selectVisualiser:: MonadWidget t m => Dynamic t Rational -> TimeVision -> W t m ()
+selectVisualiser:: MonadWidget t m => Dynamic t Rational -> TimeVision -> W t m TimeVision
 selectVisualiser beat Cyclic = visualiseCycles beat 
 selectVisualiser beat Metric = visualiseMetre beat
 selectVisualiser beat _ = visualiseCycles beat
@@ -51,11 +51,11 @@ visualiseTempoWidget delta = divClass "tempoVisualiser" $  mdo
 
 --   segments <- get segments from slider
 
-  d <- delta 
+--  d <- delta 
 
   let visMap =  fromList [(Cyclic, "Cyclic"),(Metric, "Metric")] 
-  visChange <- _dropdown_change <$> dropdown d (constDyn visMap) (def & attributes .~ constDyn ("class" =: "ui-dropdownMenus primary-color primary-borders ui-font"))
-  visualiser <- holdDyn d visChange
+  visChange <- _dropdown_change <$> dropdown Cyclic (constDyn visMap) (def & attributes .~ constDyn ("class" =: "ui-dropdownMenus primary-color primary-borders ui-font"))
+  visualiser <- holdDyn Cyclic visChange
 
   c <- context
   let currentTempo = fmap (tempo . ensemble . ensembleC) c
@@ -65,9 +65,11 @@ visualiseTempoWidget delta = divClass "tempoVisualiser" $  mdo
 
   dynBeat <- holdDyn 0 beatPosition
 
-  selectVisualiser dynBeat <$> visualiser -- this doesnt work :(
+--  selectVisualiser dynBeat visualiser -- this doesnt work :(
 
 --  visualiseCycles dynBeat
+  visualiseRing dynBeat
+
 
   v <- variable delta visChange -- just changedx this do not forget
   return v
@@ -79,7 +81,7 @@ getElapsedBeats t now = do
 
 
 ---- separate the view Box from the circle, so this function can be a generic container for the metric and cyclic vis
-visualiseCycles :: MonadWidget t m => Dynamic t Rational -> W t m ()
+visualiseCycles :: MonadWidget t m => Dynamic t Rational -> W t m TimeVision
 visualiseCycles delta = do
   let class' = constDyn $ "class" =: "human-to-human-comm code-font"
   let style = constDyn $ "style" =: "height: auto;"
@@ -108,7 +110,7 @@ visualiseCycles delta = do
     elDynAttrNS' (Just "http://www.w3.org/2000/svg") "line" attrsLine $ return ()
     -- mark
     generatePieSegments 1
-  return ()
+  return Cyclic
 
 
 
@@ -122,7 +124,12 @@ beatToPercentage atr beat = atr =: (showt percen)
   where percen = fromIntegral (round $ percen' * 100) :: Double
         percen' = beat - (realToFrac $ floor beat)
 
-visualiseMetre :: MonadWidget t m => Dynamic t Rational -> W t m ()
+beatToSegment:: Rational -> Rational -> Map Text Text
+beatToSegment nSegments beat = "stroke-dashoffset" =: (showt percen)
+  where percen = fromIntegral (round $ percen' * 100) :: Double
+        percen' = beat - (realToFrac $ floor beat)
+
+visualiseMetre :: MonadWidget t m => Dynamic t Rational -> W t m TimeVision
 visualiseMetre delta = do
   let class' = constDyn $ "class" =: "human-to-human-comm code-font"
   let style = constDyn $ "style" =: "height: auto;"
@@ -146,7 +153,7 @@ visualiseMetre delta = do
     elDynAttrNS' (Just "http://www.w3.org/2000/svg") "line" attrsLine $ return ()
     -- mark
     generateSegments 100 4
-  return ()
+  return Metric
 
 
 -- simpleList :: MonadWidget t m => Dynamic t [v] -> (Dynamic t v -> m a) -> m (Dynamic t [a]) 
@@ -188,9 +195,85 @@ generatePieSegment x = do
   let (x1,x2) = (constDyn $ "x1" =: "0",constDyn $ "x2" =: "0")
   let (y1,y2) = (constDyn $ "y1" =: "0",constDyn $ "y2" =: "-1")
   let transform = (\x -> "transform" =: ("rotate(" <> (showt (realToFrac x :: Double)) <> ")")) <$> x
-
   let attrsLine = mconcat [x1,y1,x2,y2,stroke,strokeWidth,transform]
-
   elDynAttrNS' (Just "http://www.w3.org/2000/svg") "line" attrsLine $ return ()
-  
   return ()
+
+
+visualiseRing :: MonadWidget t m => Dynamic t Rational -> W t m ()
+visualiseRing delta = do
+  let class' = constDyn $ "class" =: "human-to-human-comm code-font"
+  let style = constDyn $ "style" =: "height: auto;"
+  let vB = constDyn $ "viewBox" =: "0 0 100 100"
+  let w' = constDyn $ "width" =: "100"
+  let h' = constDyn $ "height" =: "100"
+  let attrs = mconcat [class',w',h',style,vB]
+
+  let stroke = constDyn $ "stroke" =: "var(--secondary-color)"
+  let strokeWidth = constDyn $ "stroke-width" =: "6"
+  let fill = constDyn $ "fill" =: "transparent"
+  let cx = constDyn $  "cx" =: "50" 
+  let cy = constDyn $  "cy" =: "50"
+  let r = constDyn $ "r" =: "30"
+  let dashArray = constDyn $ "stroke-dasharray" =: "25 75" 
+  let offset = beatToSegment 4 <$> delta
+
+
+  let currentBeatAttrs = mconcat [class',cx,cy,r,fill,stroke,strokeWidth,dashArray,offset]
+
+ -- elDynAttr "stopwatch" attrs $ dynText $ fmap (showt) $ fmap (showt) delta
+  elDynAttrNS' (Just "http://www.w3.org/2000/svg") "svg" attrs $ do
+    ring
+    generateRingSegments 4
+    elDynAttrNS' (Just "http://www.w3.org/2000/svg") "circle" currentBeatAttrs $ return ()
+  return ()
+
+
+ring:: MonadWidget t m => W t m ()
+ring = do
+  let class' = constDyn $ "class" =: "human-to-human-comm code-font"
+  let cx = constDyn $  "cx" =: "50" 
+  let cy = constDyn $  "cy" =: "50"
+  let r = constDyn $ "r" =: "30"
+  let fill = constDyn $ "fill" =: "transparent"
+  let (stroke,strokew) = (constDyn $ "stroke" =: "var(--primary-color)",constDyn $ "stroke-width" =: "7")
+  let ringAttrs = mconcat [class',cx,cy,r,fill,stroke,strokew]
+  let holeAttrs = mconcat [class',cx,cy,r,fill]
+  elDynAttrNS' (Just "http://www.w3.org/2000/svg") "circle" ringAttrs $ return ()
+  elDynAttrNS' (Just "http://www.w3.org/2000/svg") "circle" holeAttrs $ return ()
+  return ()
+
+generateRingSegments:: MonadWidget t m => Rational ->  m ()
+generateRingSegments nSegs = do
+  let segmentsSize = 100 / nSegs 
+      segList = constDyn $ Prelude.take (floor nSegs) $ iterate (+ segmentsSize) 0
+  x <- simpleList segList (generateRingSegment)
+  return ()
+  
+generateRingSegment:: MonadWidget t m => Dynamic t Rational ->  m ()
+generateRingSegment x = do
+  let stroke = constDyn $ "stroke" =: "var(--background-color)"
+  let strokeWidth = constDyn $ "stroke-width" =: "4"
+  let fill = constDyn $ "fill" =: "transparent"
+  let cx = constDyn $  "cx" =: "50" 
+  let cy = constDyn $  "cy" =: "50"
+  let r = constDyn $ "r" =: "30"
+  let dashArray = constDyn $ "stroke-dasharray" =: "25 75" 
+  let offset = (\x -> "stroke-dashoffset" =: (showt (realToFrac x :: Double))) <$> x
+  
+  let attrs = mconcat [cx,cy,r,stroke,strokeWidth,fill,dashArray,offset]
+  elDynAttrNS' (Just "http://www.w3.org/2000/svg") "circle" attrs $ return ()
+  return ()
+
+
+
+--   <circle class="donut-segment" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="black" stroke-width="3" stroke-dasharray="25 75" stroke-dashoffset="0"></circle>
+--   <circle class="donut-segment" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="black" stroke-width="3" stroke-dasharray="25 75" stroke-dashoffset="25"></circle>
+--   <circle class="donut-segment" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="black" stroke-width="3" stroke-dasharray="25 75" stroke-dashoffset="50"></circle>
+--   <circle class="donut-segment" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="black" stroke-width="3" stroke-dasharray="25 75" stroke-dashoffset="75"></circle>
+
+
+-- <svg width="100%" height="100%" viewBox="0 0 42 42" class="donut">
+--   <circle class="donut-hole" cx="21" cy="21" r="15.91549430918954" fill="#fff"></circle>
+--   <circle class="donut-ring" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="green" stroke-width="4"></circle>
+-- </svg>
