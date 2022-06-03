a5d4fca0b5ecdcd9fe303044471ed0d7d5b3add2
David Ogborn <ogbornd@mcmaster.ca>
2022-06-03 19:01:52 -0400

    LocoMotion update, including new general purpose module Estuary.Render.ForeignTempo

--- client/src/Estuary/Languages/LocoMotion.hs
+++ client/src/Estuary/Languages/LocoMotion.hs
@@ -5,12 +5,14 @@ module Estuary.Languages.LocoMotion (locoMotion) where
 import Data.Text
 import Data.Time
 import qualified Data.IntMap as IntMap
-import GHCJS.DOM.Types (HTMLCanvasElement,JSVal)
+import GHCJS.DOM.Types hiding (Text)
 import Control.Monad.State.Strict
 
-import Estuary.Types.TextNotation
+import Estuary.Types.TextNotation as TextNotation
 import Estuary.Types.Context
+import Estuary.Types.Tempo
 import Estuary.Render.R
+import Estuary.Render.ForeignTempo
 import Estuary.Render.TextNotationRenderer
 import Estuary.Types.RenderState
 
@@ -30,20 +32,19 @@ parseZone' ctx z txt eTime = do
   let cvs = locoMotionCanvas s
   j <- case IntMap.lookup z (locoMotions s) of
     Nothing -> do
-      j <- liftIO $ launch cvs
+      j <- liftIO $ newLocoMotion cvs
       modify' $ \s -> s { locoMotions = IntMap.insert z j $ locoMotions s }
       return j
     Just x -> return x
-  r <- liftIO $ evaluateLocoMotion j txt
+  r <- liftIO $ evaluate j txt
   case r of
     Just err -> do
-      setBaseNotation z LocoMotion
+      setBaseNotation z TextNotation.LocoMotion
       setZoneError z err
     Nothing -> do
-      setBaseNotation z LocoMotion
+      setBaseNotation z TextNotation.LocoMotion
       setEvaluationTime z eTime
       clearZoneError z
-
 
 
 clearZone'' :: Int -> R ()
@@ -54,25 +55,25 @@ zoneAnimationFrame' :: UTCTime -> Int -> R ()
 zoneAnimationFrame' now z = do
   s <- get
   case IntMap.lookup z (locoMotions s) of
-    Just x -> liftIO $ animateLocoMotion x
+    Just lm -> liftIO $ setTempo lm (tempoCache s) >> animate lm
     Nothing -> return ()
 
 
 foreign import javascript safe
-  "locoMotion.launch($1)"
-  launch :: HTMLCanvasElement -> IO JSVal
+  "new LocoMotion.LocoMotion($1)"
+  newLocoMotion :: HTMLCanvasElement -> IO LocoMotion
 
 -- Nothing values represent successful evaluation
-evaluateLocoMotion :: JSVal -> Text -> IO (Maybe Text)
-evaluateLocoMotion j x = do
-  x <- _evaluateLocoMotion j x
+evaluate :: LocoMotion -> Text -> IO (Maybe Text)
+evaluate lm x = do
+  x <- _evaluate lm x
   case _success x of
     True -> return Nothing
     False -> return $ Just $ _error x
 
 foreign import javascript safe
-  "locoMotion.evaluateLocomotion($1,$2)"
-  _evaluateLocoMotion :: JSVal -> Text -> IO JSVal
+  "$1.evaluate($2)"
+  _evaluate :: LocoMotion -> Text -> IO JSVal
 
 foreign import javascript safe
   "$1.success"
@@ -83,5 +84,12 @@ foreign import javascript safe
   _error :: JSVal -> Text
 
 foreign import javascript unsafe
-  "locoMotion.animateLocomotion($1)"
-  animateLocoMotion :: JSVal -> IO ()
+  "$1.animate()"
+  animate :: LocoMotion -> IO ()
+
+foreign import javascript unsafe
+  "$1.setTempo($2)"
+  _setTempo :: LocoMotion -> ForeignTempo -> IO ()
+
+setTempo :: LocoMotion -> Tempo -> IO ()
+setTempo lm x = _setTempo lm $ toForeignTempo x
--- client/src/Estuary/Render/TextNotationRenderer.hs
+++ client/src/Estuary/Render/TextNotationRenderer.hs
@@ -4,12 +4,13 @@ import Data.Text
 import Data.Time
 
 import Estuary.Types.Context
+import Estuary.Types.Tempo
 import Estuary.Render.R
 
 data TextNotationRenderer = TextNotationRenderer {
   parseZone :: Context -> Int -> Text -> UTCTime -> R (),
   clearZone' :: Int -> R (),
-  preAnimationFrame :: R (),
   zoneAnimationFrame :: UTCTime -> Int -> R (),
+  preAnimationFrame :: R (),
   postAnimationFrame :: R ()
 }
