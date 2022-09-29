f188aba7b0ffa3e9590aae0c6e2587358ea09c84
David Ogborn <ogbornd@mcmaster.ca>
2022-09-29 11:21:50 -0400

    LogEntry reworked as generic record type to be used for all types of log entries

--- common/src/Estuary/Types/LogEntry.hs
+++ common/src/Estuary/Types/LogEntry.hs
@@ -2,30 +2,20 @@
 
 module Estuary.Types.LogEntry where
 
-import Data.Maybe (mapMaybe,catMaybes)
 import Data.Time.Clock
 import Data.Text
 import GHC.Generics
 import Data.Aeson
 
-import Estuary.Utility
-import Estuary.Types.Definition
-import Estuary.Types.Participant
-import Estuary.Types.Chat
 import Estuary.Types.TranslatableText
 
-data LogEntry =
-  LogChat Chat |
-  EnsembleEvent UTCTime TranslatableText -- eg. "so-and-so has joined the ensemble"
-  deriving (Generic,Eq)
+data LogEntry = LogEntry {
+  logEntryTime :: UTCTime,
+  logEntrySender :: Text, -- empty for everything except chat messages
+  logEntryText :: TranslatableText
+  }
+  deriving (Generic,Eq,Ord)
 
 instance ToJSON LogEntry where
   toEncoding = genericToEncoding defaultOptions
 instance FromJSON LogEntry
-
-instance Ord LogEntry where
-  compare (LogChat c1) (LogChat c2) = compare (chatTime c1) (chatTime c2)
-  compare (EnsembleEvent t1 _) (EnsembleEvent t2 _) = compare t1 t2
-  compare (LogChat c) (EnsembleEvent t _) = compare (chatTime c) t
-  compare (EnsembleEvent t _) (LogChat c) = compare t (chatTime c)
-  
